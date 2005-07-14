0385b0366a057c09d180518d0454e3671fa93dd1
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2005-07-14 16:03:18 +0000

    Added /weblog/ to django_website main urlconf
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@16 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/settings/urls/main.py
+++ django_website/settings/urls/main.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    (r'^weblog/', include('django_website.apps.blog.urls.blog')),
     (r'', include('django.conf.urls.flatfiles')),
 )
