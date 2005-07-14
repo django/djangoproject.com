b0e33db366dd2347431ed6c2ff8717ab757e6f92
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2005-07-14 16:33:22 +0000

    Added basic documentation app to tie into the django docs
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@20 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django_website/apps/docs/urls/docs.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+# infodict for generic view
+info = {
+    'app_label'     : 'docs',
+    'module_name'   : 'documents',
+    'slug_field'    : 'slug',
+}
+
+urlpatterns = patterns('django.views.generic.list_detail',
+    (r'^(?P<slug>\w+)', 'object_detail', info),
+)
