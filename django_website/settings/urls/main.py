40fe7f8c58c992a8bb14567796ef3087cadf2610
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2005-07-14 16:35:56 +0000

    Added docs to django_website main urls
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@21 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/settings/urls/main.py
+++ django_website/settings/urls/main.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^weblog/', include('django_website.apps.blog.urls.blog')),
+    (r'^documentation/', include('django_website.apps.docs.urls.docs')),
     (r'', include('django.conf.urls.flatfiles')),
 )
