13b3c496ffe6edcef381510084012f3691cc541d
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2005-07-14 15:55:02 +0000

    Added blog app to django_website
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@13 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django_website/apps/blog/models/blog.py
@@ -0,0 +1,25 @@
+from django.core import meta
+
+class Entry(meta.Model):
+    verbose_name_plural = 'entries'
+    module_name = 'entries'
+    db_table = 'new_blog_entries'
+    fields = (
+        meta.DateTimeField('pub_date', 'publication date'),
+        meta.SlugField('slug', 'slug', unique_for_date='pub_date'),
+        meta.CharField('headline', 'headline', maxlength=200),
+        meta.TextField('body', 'body', help_text="Use raw HTML."),
+    )
+    ordering = (('pub_date', 'DESC'),)
+    admin = meta.Admin(
+        fields = (
+            (None, {'fields': ('pub_date', 'slug', 'headline', 'body')}),
+        ),
+        list_display = ('pub_date', 'headline'),
+    )
+
+    def __repr__(self):
+        return self.headline
+
+    def get_absolute_url(self):
+        return "/weblog/%s/%s/" % (self.pub_date.strftime("%Y/%b/%d"), self.slug)
