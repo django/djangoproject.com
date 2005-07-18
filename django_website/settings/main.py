64589dba4d541bdf0c4a08f605096e5ddb1a9a8b
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2005-07-18 19:15:03 +0000

    Checking in some extra settings for djangoproject.com main
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@180 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/settings/main.py
+++ django_website/settings/main.py
@@ -1,9 +1,18 @@
+ADMINS = (('Adrian Holovaty','aholovaty@ljworld.com'), ('Jacob Kaplan-Moss', 'jacob@lawrence.com'))
+TIME_ZONE = 'America/Chicago'
+
 SERVER_EMAIL = 'root@pam.servers.ljworld.com'
 MANAGERS = (('Wilson Miner','wminer@ljworld.com'),)
 
-DEBUG = True
+DEBUG = False
 PREPEND_WWW = True
+
+DATABASE_ENGINE = 'postgresql'
 DATABASE_NAME = 'djangoproject'
+DATABASE_USER = 'apache'
+DATABASE_PASSWORD = ''
+DATABASE_HOST = '' # set to empty string for localhost
+
 SITE_ID = 1
 TEMPLATE_DIRS = (
     '/home/html/templates/djangoproject.com/',
