5102c294c1306d016abea94456aa2db2c86ddae9
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2005-08-01 19:12:32 +0000

    Added latest-comments RSS feed to djangoproject.com
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@368 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/settings/main_rss.py
+++ django_website/settings/main_rss.py
@@ -1,5 +1,6 @@
 from django.core import rss
 from django.models.blog import entries
+from django.models.comments import freecomments
 
 weblog_entry_feed = rss.FeedConfiguration(
     slug = 'weblog',
@@ -12,4 +13,16 @@ weblog_entry_feed = rss.FeedConfiguration(
     }
 )
 
+recent_comments = rss.FeedConfiguration(
+    slug = 'comments',
+    title_cb = lambda param: "Recent comments on djangoproject.com",
+    link_cb = lambda param: "http://www.djangoproject.com/comments/",
+    description_cb = lambda param: "Recent comments posted to djangoproject.com.",
+    get_list_func_cb = lambda obj: freecomments.get_list,
+    get_list_kwargs = {
+        'limit': 15,
+    }
+)
+
+rss.register_feed(recent_comments)
 rss.register_feed(weblog_entry_feed)
