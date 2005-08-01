233e9fe164cb7ab34319fc4aadd50af3140fb911
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2005-08-01 18:46:24 +0000

    Fixed #149 -- djangoproject.com/comments/ now displays latest comments
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@361 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/settings/urls/main.py
+++ django_website/settings/urls/main.py
@@ -1,8 +1,15 @@
 from django.conf.urls.defaults import *
 
+info_dict = {
+    'app_label': 'comments',
+    'module_name': 'freecomments',
+    'paginate_by': 15,
+}
+
 urlpatterns = patterns('',
     (r'^weblog/', include('django_website.apps.blog.urls.blog')),
     (r'^documentation/', include('django_website.apps.docs.urls.docs')),
+    (r'^comments/$', 'django.views.generic.list_detail.object_list', info_dict),
     (r'^comments/', include('django.contrib.comments.urls.comments')),
     (r'^rss/', include('django.conf.urls.rss')),
     (r'', include('django.conf.urls.flatfiles')),
