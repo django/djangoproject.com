d59a81d6775785db78ece56c2917a7394bae6ec1
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2005-08-06 23:54:06 +0000

    Improved djangoproject community aggregator
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@422 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/apps/aggregator/bin/update_feeds.py
+++ django_website/apps/aggregator/bin/update_feeds.py
@@ -12,20 +12,31 @@ import feedparser
 def update_feeds():
     from django.models.aggregator import feeds, feeditems
     for feed in feeds.get_iterator(is_defunct__exact=False):
-        for entry in feedparser.parse(feed.feed_url).entries:
-            try:
-                feeditem = feed.get_feeditem(guid__exact=str(entry.get("id", entry.link)))
-            except feeditems.FeedItemDoesNotExist:
-                for i in ("summary", "content", "description"):
-                    summary = entry.get(i, "")
-                    if summary: break
-                feeditem = feed.add_feeditem(title=str(entry.title),
-                                             link=str(entry.link),
-                                             summary=str(summary),
-                                             guid=str(entry.get("id", entry.link)),
-                                             date_modified=datetime.datetime.fromtimestamp(time.mktime(entry.modified_parsed)))
+        parsed_feed = feedparser.parse(feed.feed_url)
+        for entry in parsed_feed.entries:
+            title = entry.title.encode(parsed_feed.encoding, "xmlcharrefreplace")
+            guid = entry.get("id", entry.link).encode(parsed_feed.encoding, "xmlcharrefreplace")
+            link = entry.link.encode(parsed_feed.encoding, "xmlcharrefreplace")
+            if hasattr(entry, "summary"):
+                content = entry.summary
+            elif hasattr(entry, "content"):
+                content = entry.content[0].value
+            elif hasattr(entry, "description"):
+                content = entry.description
             else:
-                feeditem.date_modified = datetime.datetime.fromtimestamp(time.mktime(entry.modified_parsed))
+                content = u""
+            content = content.encode(parsed_feed.encoding, "xmlcharrefreplace")
+            date_modified = datetime.datetime.fromtimestamp(time.mktime(entry.modified_parsed))
+            try:
+                feeditem = feed.get_feeditem(guid__exact=guid)
+            except feeditems.FeedItemDoesNotExist:
+                feeditem = feed.add_feeditem(title=title,
+                                             link=link,
+                                             summary=content,
+                                             guid=guid,
+                                             date_modified=date_modified)
+            else:
+                feeditem.date_modified = date_modified
                 feeditem.save()
 
 if __name__ == '__main__':
