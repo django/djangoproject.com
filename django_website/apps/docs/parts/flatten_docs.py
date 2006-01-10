f9715d734d9d879402b002bf6e09539b1ad51e86
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2006-01-10 23:18:31 +0000

    Added django_website/apps/docs/parts/flatten_docs.py
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@1897 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django_website/apps/docs/parts/flatten_docs.py
@@ -0,0 +1,20 @@
+"""
+Utility that converts all current Django documents to flat pages.
+
+We use this to flatten/freeze the current Django documentation for
+a particular version.
+"""
+
+from django.models.flatpages import flatpages
+from django.models.docs import documents
+
+for doc in documents.get_list():
+    f = flatpages.FlatPage(
+        url='/documentation/0_90/%s/' % doc.slug,
+        title='Documentation (version 0.90) | %s' % doc.title,
+        content='%s</div><div id="content-related" class="sidebar"><h2>Contents</h2>%s' % (doc.get_content(), doc.get_toc()),
+        enable_comments=False,
+        template_name='flatfiles/legacy_docs',
+        registration_required=False)
+    f.save()
+    f.set_sites([1])
