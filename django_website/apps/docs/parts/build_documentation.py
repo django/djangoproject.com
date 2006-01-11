e7984eb3f5ee034eb7cfeb7855d2bc9deb863ce5
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2006-01-11 04:53:39 +0000

    Changed django_website/apps/docs/parts/build_documentation.py to add link to old version for all docs
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@1902 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/apps/docs/parts/build_documentation.py
+++ django_website/apps/docs/parts/build_documentation.py
@@ -212,7 +212,7 @@ class DjangoHTMLTranslator(html4css1.HTMLTranslator):
             close_tag = '</caption>\n'
         elif isinstance(node.parent, nodes.document):
             self.body.append(self.starttag(node, 'h1', '', CLASS='title'))
-            self.context.append('</h1>\n')
+            self.context.append('</h1>\n<h2 class="deck">This covers the Django development version. Old version: <a href="/documentation/0_90/">0.90 docs</a></h2>\n')
             self.in_document_title = len(self.body)
         else:
             assert isinstance(node.parent, nodes.section)
