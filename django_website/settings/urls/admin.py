bec7339dd276eed948983cd0c9d0f89064b21bf0
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2006-05-30 01:42:34 +0000

    Fixed path to admin URLconf in djangoproject.com admin settings
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@3005 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/settings/urls/admin.py
+++ django_website/settings/urls/admin.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^password_reset/', include('django.conf.urls.admin_password_reset')),
     (r'^r/', include('django.conf.urls.shortcut')),
-    (r'', include('django.conf.urls.admin')),
+    (r'', include('django.contrib.admin.urls.admin')),
 )
