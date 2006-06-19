c06467a90a3d3ea9073940c043c6504888b208ee
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2006-06-19 19:13:04 +0000

    Moved things around in djangoproject.com project to match the standard Django layout
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@3154 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/settings/urls/admin.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^password_reset/', include('django.conf.urls.admin_password_reset')),
-    (r'^r/', include('django.conf.urls.shortcut')),
-    (r'', include('django.contrib.admin.urls.admin')),
-)
