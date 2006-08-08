7d344a1fe0723c3147d6ca007d466f95fe340abe
adrian <adrian@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2006-08-08 04:51:28 +0000

    Changed WeblogEntryFeed in django_website.blog app to ignore future entries
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@3535 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/apps/blog/feeds.py
+++ django_website/apps/blog/feeds.py
@@ -1,5 +1,6 @@
 from django.contrib.syndication.feeds import Feed
 from django_website.apps.blog.models import Entry
+import datetime
 
 class WeblogEntryFeed(Feed):
     title = "The Django weblog"
@@ -7,4 +8,4 @@ class WeblogEntryFeed(Feed):
     description = "Latest news about Django, the Python Web framework."
 
     def items(self):
-        return Entry.objects.all()[:10]
+        return Entry.objects.filter(pub_date__lte=datetime.datetime.now())[:10]
