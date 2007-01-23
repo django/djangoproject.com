c190f51bba08b56333ee5d06834ab9d428042d9b
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2007-01-23 16:27:59 +0000

    Checking in latest changes to djangoproject.com templates
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@4401 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/templates/trac_templates/ticket_notify_email.cs
+++ django_website/templates/trac_templates/ticket_notify_email.cs
@@ -1,23 +1,23 @@
-<?cs var:$email.ticket_body_hdr ?>
-<?cs var:$email.ticket_props ?><?cs 
-if:$ticket.new ?>
-<?cs var:$ticket.description ?>
+<?cs var:email.ticket_body_hdr ?>
+<?cs var:email.ticket_props ?><?cs 
+if:ticket.new ?>
+<?cs var:ticket.description ?>
 <?cs else ?><?cs 
- if:$email.changes_body ?>
-Changes (by <?cs var:$ticket.change.author ?>):
+ if:email.changes_body ?>
+Changes (by <?cs var:ticket.change.author ?>):
 
-<?cs var:$email.changes_body ?><?cs
+<?cs var:email.changes_body ?><?cs
  /if ?><?cs 
-var:$email.changes_descr 
-?><?cs if:$ticket.change.comment.newvalue ?>
+var:email.changes_descr 
+?><?cs if:ticket.change.comment ?>
 Comment<?cs 
- if:!$email.changes_body ?> (by <?cs 
-   var:$ticket.change.comment.author ?>)<?cs /if ?>:
+ if:!email.changes_body ?> (by <?cs 
+   var:ticket.change.author ?>)<?cs /if ?>:
 
-<?cs var:$ticket.change.comment.newvalue ?>
+<?cs var:ticket.change.comment ?>
 <?cs /if ?><?cs 
 /if ?>
 -- 
-Ticket URL: <<?cs var:$ticket.link ?>>
-<?cs var:$project.name ?> <<?cs var:$project.url ?>>
-<?cs var:$project.descr ?>
+Ticket URL: <<?cs var:ticket.link ?>>
+<?cs var:project.name ?> <<?cs var:project.url ?>>
+<?cs var:project.descr ?>
