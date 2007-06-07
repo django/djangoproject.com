426e041e873e4fa274a3c663c378c235fd0d7a5d
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2007-06-07 16:49:31 +0000

    Added a tool to mark defunct feeds.
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@5430 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django_website/apps/aggregator/bin/mark_defunct_feeds.py
@@ -0,0 +1,14 @@
+"""
+Mark people with 404'ing feeds as defunct.
+"""
+
+for f in Feed.objects.all():
+    try:
+        r = urllib2.urlopen(f.feed_url)
+    except urllib2.HTTPError, e:
+        if e.code == 404 or e.code == 500:
+            print "%s on %s; marking defunct" % f
+            f.is_defunct = True
+            f.save()
+        else:
+            raise
