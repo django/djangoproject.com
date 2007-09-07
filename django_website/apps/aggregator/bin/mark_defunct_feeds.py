0c2ed811ff7fba890df287860e14e527c837dd09
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2007-09-07 18:39:10 +0000

    [django-website] small bug fix to aggregator
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@6061 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/apps/aggregator/bin/mark_defunct_feeds.py
+++ django_website/apps/aggregator/bin/mark_defunct_feeds.py
@@ -7,7 +7,7 @@ for f in Feed.objects.all():
         r = urllib2.urlopen(f.feed_url)
     except urllib2.HTTPError, e:
         if e.code == 404 or e.code == 500:
-            print "%s on %s; marking defunct" % f
+            print "%s on %s; marking defunct" % (e.code, f)
             f.is_defunct = True
             f.save()
         else:
