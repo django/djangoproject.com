f7fe26136476fb2cf766a7e071bcf282c2fb139a
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2008-08-22 19:20:07 +0000

    [djangoproject.com] Various template tweaks.
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@8468 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- djangodocs/settings.py
+++ djangodocs/settings.py
@@ -4,6 +4,7 @@ PREPEND_WWW = False
 INSTALLED_APPS = []
 TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__), "templates")] + TEMPLATE_DIRS
 ROOT_URLCONF = 'djangodocs.urls'
+CACHE_MIDDLEWARE_KEY_PREFIX = 'djangodocs'
 
 if DEVELOPMENT_MODE:
     DOCS_PICKLE_ROOT = "/Users/jacob/Projects/Django/upstream/docs/_build/pickle/"
