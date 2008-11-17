24b163b7b7ac33b4f964aaa2d3b47096adaf5f13
jacob <jacob@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2008-11-17 19:48:29 +0000

    [djangoproject.com] Fixed password resets.
    git-svn-id: http://code.djangoproject.com/svn/djangoproject.com@9486 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django_website/apps/accounts/urls.py
+++ django_website/apps/accounts/urls.py
@@ -9,5 +9,14 @@ urlpatterns = patterns('',
         {'form_class': RegistrationFormUniqueEmail},
         name='registration_register',
     ),
+    url(
+        r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+        'django.contrib.auth.views.password_reset_confirm',
+        name='auth_password_reset_confim',
+    ),
+    url(
+        r'^reset/done/$', 
+        'django.contrib.auth.views.password_reset_complete'
+    ),
     (r'', include('registration.urls')),
 )
