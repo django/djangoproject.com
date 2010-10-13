4a810640a3ccdaaa975b9d1b807ff7365282e5b9
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-10-13 11:47:18 -0500

    Fix the docbuilds path location.

--- django_website/settings/docs.py
+++ django_website/settings/docs.py
@@ -9,6 +9,6 @@ CACHE_MIDDLEWARE_KEY_PREFIX = 'djangodocs'
 
 # Where to store the build Sphinx docs.
 if PRODUCTION:
-    DOCS_BUILD_ROOT = BASE.parent.child('docbuilds')
+    DOCS_BUILD_ROOT = BASE.ancestor(2).child('docbuilds')
 else:
     DOCS_BUILD_ROOT = '/tmp/djangodocs'
