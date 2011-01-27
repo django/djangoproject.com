95d533ed9b9e8535584b0cda93652bbddd95b18d
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-01-27 16:28:18 -0600

    Made the manage.py work for the docs site too.

--- manage.py
+++ manage.py
@@ -4,6 +4,11 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
-import django_website.settings.www
+if '--docs' in sys.argv:
+    import django_website.settings.docs as settings
+    sys.argv.remove('--docs')
+else:
+    import django_website.settings.www as settings
+    
 from django.core.management import execute_manager
-execute_manager(django_website.settings.www)
+execute_manager(settings)
