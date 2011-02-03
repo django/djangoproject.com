7c2eddac2b110f8873fbc083e4a3d529c4bb647d
Justin Lilly <justin@justinlilly.com>
2011-02-03 01:08:36 -0500

    Using the models rather than hardcoded values.

--- django_website/context_processors.py
+++ django_website/context_processors.py
@@ -1,4 +1,11 @@
 from django.conf import settings
+from django.core.cache import cache
+
+from django_website.docs.models import DocumentRelease
 
 def recent_release(request):
-    return {'RECENT_RELEASE': settings.RECENT_RELEASE}
+    recent_release = cache.get('recent_release')
+    if not recent_release:
+        recent_release = DocumentRelease.objects.default().version
+        cache.set('recent_release', recent_release)
+    return {'RECENT_RELEASE': recent_release}
