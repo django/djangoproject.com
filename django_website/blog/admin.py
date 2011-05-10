83b5ab8a3aeb32c55cedc1eff49676c88bb795ed
Julien Phalip <jphalip@gmail.com>
2011-05-10 19:46:32 +1000

    Use proper ModelAdmin for blog entry

--- django_website/blog/admin.py
+++ django_website/blog/admin.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 
 from .models import Entry
 
-admin.site.register(Entry,
-    list_display = ('headline', 'pub_date', 'is_active', 'is_published', 'author'),
-    list_filter = ('is_active',),
-    exclude = ('summary_html', 'body_html'),
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'pub_date', 'is_active', 'is_published', 'author')
+    list_filter = ('is_active',)
+    exclude = ('summary_html', 'body_html')
     prepopulated_fields = {"slug": ("headline",)}
-)
+
+admin.site.register(Entry, EntryAdmin)
