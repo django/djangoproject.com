b3b58fcfc19d2378063cf32f1376e1bad9770674
Julien Phalip <jphalip@gmail.com>
2011-05-10 19:30:34 +1000

    Format blog entry's summary and body with RST. No moar raw HTML!

--- django_website/blog/admin.py
+++ django_website/blog/admin.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 
 from django.contrib import admin
+
 from .models import Entry
 
 admin.site.register(Entry,
     list_display = ('headline', 'pub_date', 'is_active', 'is_published', 'author'),
-    list_filter = ('is_active',)
+    list_filter = ('is_active',),
+    exclude = ('summary_html', 'body_html'),
 )
