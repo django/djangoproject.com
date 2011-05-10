85f169732b8766696ca3a23493ecce56408b778d
Julien Phalip <jphalip@gmail.com>
2011-05-10 19:37:33 +1000

    Make sure only published entries are publicly accessible.

--- django_website/blog/urls.py
+++ django_website/blog/urls.py
@@ -9,7 +9,7 @@ from .models import Entry
 def prepare_arguments(view):
     def wrapped(request, *args, **kwargs):
         kwargs['allow_future'] = request.user.is_staff
-        kwargs['queryset'] = Entry.objects.all() if request.user.is_staff else Entry.objects.active()
+        kwargs['queryset'] = Entry.objects.all() if request.user.is_staff else Entry.objects.published()
         kwargs['date_field'] = 'pub_date'
         return view(request, *args, **kwargs)
     return wrapped
