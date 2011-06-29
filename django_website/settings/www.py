60ebdb90b82c872ff3c369bb04e8bd88d9eb4733
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-06-29 16:27:35 -0500

    Upgraded to Django 1.3.

--- django_website/settings/www.py
+++ django_website/settings/www.py
@@ -86,8 +86,8 @@ if PRODUCTION:
     MIDDLEWARE_CLASSES.append('django.middleware.cache.FetchFromCacheMiddleware')
 
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 TEMPLATE_CONTEXT_PROCESSORS = [
     "django.contrib.auth.context_processors.auth",
