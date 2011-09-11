bb010b9438a007a934aa1ed3a1d379d5ae1792fd
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-09-11 13:32:55 -0500

    Added CLA info and gravator/robohash to user profile pages.

--- django_website/accounts/views.py
+++ django_website/accounts/views.py
@@ -1,7 +1,14 @@
+import hashlib
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from ..cla.models import find_agreements
 
 def user_profile(request, username):
     u = get_object_or_404(User, username=username)
-    ctx = {'user': u, 'user_can_commit': u.has_perm('auth.commit')}
+    ctx = {
+        'user': u,
+        'email_hash': hashlib.md5(u.email).hexdigest(),
+        'user_can_commit': u.has_perm('auth.commit'),
+        'clas': find_agreements(u),
+    }
     return render(request, "accounts/user_profile.html", ctx)
