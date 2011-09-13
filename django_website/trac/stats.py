812f64a06d93a03600d4d3098e29cb3e32f0a167
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-09-13 14:41:31 -0500

    Added some silly statistics to the user pages.
    Also cleaned up the styles slightly.

---
+++ django_website/trac/stats.py
@@ -0,0 +1,60 @@
+"""
+Various queries for grabbing interesting user stats from Trac.
+"""
+
+from __future__ import absolute_import
+import operator
+import django.db
+from .models import Revision, Ticket, TicketChange
+
+_statfuncs = []
+
+def stat(title):
+    """
+    Register a function as a "stat"
+
+    The function should take a username and return a number.
+    """
+
+    def _inner(f):
+        _statfuncs.append(f)
+        f.title = title
+        return f
+    return _inner
+
+def get_user_stats(username):
+    stats = {}
+    for func in sorted(_statfuncs, key=operator.attrgetter('title')):
+        stats[func.title] = func(username)
+    return stats
+
+@stat('Commits')
+def commit_count(username):
+    return Revision.objects.filter(author=username).count()
+
+@stat('Tickets closed')
+def tickets_closed(username):
+    # Raw query so that we can do COUNT(DISTINCT ticket).
+    q = """SELECT COUNT(DISTINCT ticket) FROM ticket_change
+           WHERE author = %s AND field = 'status' AND newvalue = 'closed';"""
+    return run_single_value_query(q, username)
+
+@stat('Tickets opened')
+def tickets_opened(username):
+    return Ticket.objects.filter(reporter=username).count()
+
+@stat('New tickets reviewed')
+def new_tickets_reviewed(username):
+    # We don't want to de-dup as for tickets_closed: multiple reviews of the
+    # same ticket should "count" as a review.
+    qs = TicketChange.objects.filter(author=username, field='stage', oldvalue='Unreviewed')
+    qs = qs.exclude(newvalue='Unreviewed')
+    return qs.count()
+
+def run_single_value_query(query, *params):
+    """
+    Helper: run a query returning a single value (e.g. a COUNT) and return the value.
+    """
+    c = django.db.connections['trac'].cursor()
+    c.execute(query, params)
+    return c.fetchone()[0]
