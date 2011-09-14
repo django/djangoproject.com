133a2be9510916aa15813ebc44f0937b6466efea
Jannis Leidel <jannis@leidel.info>
2011-09-14 21:58:57 +0200

    Added flatpages middleware as directly linking to the flatpages view didn't have the wanted effect. No idea why.

--- django_website/settings/www.py
+++ django_website/settings/www.py
@@ -89,6 +89,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
 ]
 if PRODUCTION:
