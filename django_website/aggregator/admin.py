0ba9a1cea6a642bf14c372d7ef96173c961920cf
Justin Lilly <justin@justinlilly.com>
2012-02-03 00:22:30 -0800

    Adding approval_status to Feed objects (with accompanying admin).

--- django_website/aggregator/admin.py
+++ django_website/aggregator/admin.py
@@ -3,16 +3,16 @@ from __future__ import absolute_import
 from django.contrib import admin
 from .models import Feed, FeedItem, FeedType
 
-admin.site.register(Feed, 
-    list_display  = ["title", "feed_type", "public_url"],
-    list_filter   = ["feed_type", "is_defunct"],
+admin.site.register(Feed,
+    list_display  = ["title", "feed_type", "public_url", "approval_status"],
+    list_filter   = ["feed_type", "is_defunct", "approval_status"],
     ordering      = ["title"],
     search_fields = ["title", "public_url"],
     raw_id_fields = ['owner'],
     list_per_page = 500,
 )
 
-admin.site.register(FeedItem, 
+admin.site.register(FeedItem,
     list_display   = ['title', 'feed', 'date_modified'],
     list_filter    = ['feed'],
     search_fields  = ['feed__title', 'feed__public_url', 'title'],
