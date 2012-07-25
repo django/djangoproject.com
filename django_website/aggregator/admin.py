64d78d14fe56f994df11ed4fced14ede5c57a6fa
Jannis Leidel <jannis@leidel.info>
2012-07-25 10:03:18 +0200

    Fixed PEP8 fallacies.

--- django_website/aggregator/admin.py
+++ django_website/aggregator/admin.py
@@ -19,23 +19,23 @@ mark_denied.short_description = "Mark selected feeds as denied."
 
 
 admin.site.register(Feed,
-    list_display  = ["title", "feed_type", "public_url", "approval_status"],
-    list_filter   = ["feed_type", "is_defunct", "approval_status"],
-    ordering      = ["title"],
-    search_fields = ["title", "public_url"],
-    raw_id_fields = ['owner'],
-    list_editable = ["approval_status"],
-    list_per_page = 500,
-    actions = [mark_approved, mark_denied],
+    list_display=["title", "feed_type", "public_url", "approval_status"],
+    list_filter=["feed_type", "is_defunct", "approval_status"],
+    ordering=["title"],
+    search_fields=["title", "public_url"],
+    raw_id_fields=['owner'],
+    list_editable=["approval_status"],
+    list_per_page=500,
+    actions=[mark_approved, mark_denied],
 )
 
 admin.site.register(FeedItem,
-    list_display   = ['title', 'feed', 'date_modified'],
-    list_filter    = ['feed'],
-    search_fields  = ['feed__title', 'feed__public_url', 'title'],
-    date_heirarchy = ['date_modified'],
+    list_display=['title', 'feed', 'date_modified'],
+    list_filter=['feed'],
+    search_fields=['feed__title', 'feed__public_url', 'title'],
+    date_heirarchy=['date_modified'],
 )
 
 admin.site.register(FeedType,
-    prepopulated_fields = {'slug': ('name',)},
+    prepopulated_fields={'slug': ('name',)},
 )
