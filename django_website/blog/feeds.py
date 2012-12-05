de29f27277cbcae69a0c7fc3daa5c1b5aeac3ca0
Jannis Leidel <jannis@leidel.info>
2012-12-05 10:31:22 +0100

    Fixed weblog RSS link attribute to make feed valid again.

--- django_website/blog/feeds.py
+++ django_website/blog/feeds.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import
 from django.contrib.syndication.views import Feed
 from .models import Entry
 
+
 class WeblogEntryFeed(Feed):
     title = "The Django weblog"
-    link = "http://www.djangoproject.com/weblog/"
+    link = "https://www.djangoproject.com/weblog/"
     description = "Latest news about Django, the Python Web framework."
 
     def items(self):
