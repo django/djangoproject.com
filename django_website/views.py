1fffdb60aa4eb875bfbd961773d0cf5066dc38e2
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-06 22:04:32 +0100

    Remove dead code. This isn't wired in any URLconf.

--- django_website/views.py
+++ django_website/views.py
@@ -1,30 +1,5 @@
-"""
-Misc. views.
-"""
-from __future__ import absolute_import
-
-from django.contrib.comments.models import Comment
-from django.contrib.sitemaps import views as sitemap_views
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
 from django.views.decorators.csrf import requires_csrf_token
-from django.views.generic import list_detail
-
-from .sitemaps import FlatPageSitemap, WeblogSitemap
-
-@cache_page(60*60*6)
-def sitemap(request):
-    return sitemap_views.sitemap(request, sitemaps={
-        'weblog': WeblogSitemap,
-        'flatpages': FlatPageSitemap,
-    })
-
-def comments(request):
-    return list_detail.object_list(
-        request,
-        queryset = Comment.objects.filter(is_public=True).order_by('-submit_date'),
-        paginate_by = 30,
-    )
 
 @requires_csrf_token
 def server_error(request, template_name='500.html'):
