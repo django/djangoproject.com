aa45d1777098d9f8ef89dd27a7bc2c3863395d1f
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-06 21:29:39 +0100

    Prevent the docs context processor from crashing on an empty database.

--- docs/context_processors.py
+++ docs/context_processors.py
@@ -6,7 +6,10 @@ from docs.models import DocumentRelease
 def recent_release(request):
     recent_release = cache.get('recent_release')
     if not recent_release:
-        recent_release = DocumentRelease.objects.default().version
+        try:
+            recent_release = DocumentRelease.objects.default().version
+        except DocumentRelease.DoesNotExist:
+            recent_release = 'dev'
         cache.set(
             DocumentRelease.DEFAULT_CACHE_KEY,
             recent_release,
