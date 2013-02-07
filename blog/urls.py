4e6d0c056ba1e706cad03ca9eef65fa6c446de3b
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-07 23:14:09 +0100

    Replace deprecated generic FBV with equivalent CBV in weblog.

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,20 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-   (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\w-]+)/$', views.entry_detail),
-   (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', views.archive_day),
-   (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', views.archive_month),
-   (r'^(?P<year>\d{4})/$', views.archive_year),
-   url(r'^/?$', views.archive_index, name="blog-index"),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\w-]+)/$',
+        views.BlogDateDetailView.as_view()
+    ),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
+        views.BlogDayArchiveView.as_view()
+    ),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
+        views.BlogMonthArchiveView.as_view()
+    ),
+    url(r'^(?P<year>\d{4})/$',
+        views.BlogYearArchiveView.as_view()
+    ),
+    url(r'^/?$',
+        views.BlogArchiveIndexView.as_view(),
+        name="blog-index"
+    ),
 )
