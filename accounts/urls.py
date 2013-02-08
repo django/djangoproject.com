7b5c493bd7da50efe4110730a55f88064b73c2bb
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-08 10:50:20 +0100

    Upgrade django-registration.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,21 @@
 from __future__ import absolute_import
 
 from django.conf.urls import patterns, url, include
+from django.contrib.auth import views as auth_views
+
 from registration.forms import RegistrationFormUniqueEmail
+from registration import views as registration_views
+
 from . import views as account_views
 
 urlpatterns = patterns('',
     url(
         r'^register/$',
-        "registration.views.register",
-        {'form_class': RegistrationFormUniqueEmail},
+        registration_views.register,
+        {
+            'backend': 'registration.backends.default.DefaultBackend',
+            'form_class': RegistrationFormUniqueEmail,
+        },
         name='registration_register',
     ),
     url(
@@ -18,13 +25,13 @@ urlpatterns = patterns('',
     ),
     url(
         r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        'django.contrib.auth.views.password_reset_confirm',
+        auth_views.password_reset_confirm,
         name='auth_password_reset_confim',
     ),
     url(
         r'^reset/done/$',
-        'django.contrib.auth.views.password_reset_complete'
+        auth_views.password_reset_complete,
     ),
     url(r'^_trac/userinfo/$', account_views.json_user_info),
-    (r'', include('registration.urls')),
+    url(r'', include('registration.backends.default.urls')),
 )
