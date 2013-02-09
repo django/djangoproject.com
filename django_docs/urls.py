248bc06ed8904fff30c648737a97c9a01628686f
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-09 16:46:38 +0100

    Add a sitemap for the docs site.
    Current docs have a higher priority than dev docs. Refs #18922.

--- django_docs/urls.py
+++ django_docs/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url, include
 
+from docs.sitemaps import DocsSitemap
+from docs.urls import urlpatterns as docs_urlpatterns
+
+sitemaps = {'docs': DocsSitemap}
+
 urlpatterns = patterns('',
-    url(r'', include('docs.urls')),
-)
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+) + docs_urlpatterns
