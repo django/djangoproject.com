0fd7006db661928676ec8b0547684b28d1a2b829
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-09 15:35:01 +0100

    Move docs urls inside the docs app.

--- django_docs/urls.py
+++ django_docs/urls.py
@@ -1,53 +1,5 @@
-from django.conf.urls import patterns, url
-
-from haystack.views import search_view_factory
-
-from docs import views
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('',
-    url(
-        r'^$',
-        views.index,
-    ),
-    url(
-        r'^search/$',
-        search_view_factory(view_class=views.DocSearchView),
-        name='document-search'
-    ),
-    url(
-        r'^(?P<lang>[a-z-]+)/$',
-        views.language,
-    ),
-    url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/$',
-        views.document,
-        {'url': ''},
-        name='document-index',
-    ),
-    url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_objects/$',
-        views.objects_inventory,
-        name='objects-inv',
-    ),
-    url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_images/(?P<path>.*)$',
-        views.SphinxStatic('_images'),
-    ),
-    url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_source/(?P<path>.*)$',
-        views.SphinxStatic('_sources'),
-    ),
-    url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_downloads/(?P<path>.*)$',
-        views.SphinxStatic('_downloads'),
-    ),
-    url(
-        r'^(.*)/index/$',
-        views.redirect_index,
-    ),
-    url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/(?P<url>[\w./-]*)/$',
-        views.document,
-        name='document-detail',
-    ),
+    url(r'', include('docs.urls')),
 )
