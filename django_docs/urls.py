32010f0cef9f4edf1637d41008ae86f4e864cf87
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-09 21:41:01 +0100

    Add validation URL for Google Webmaster Tools.

--- django_docs/urls.py
+++ django_docs/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url, include
+from django.http import HttpResponse
 
 from docs.sitemaps import DocsSitemap
 from docs.urls import urlpatterns as docs_urlpatterns
 
 sitemaps = {'docs': DocsSitemap}
 
-urlpatterns = patterns('',
+urlpatterns = docs_urlpatterns + patterns('',
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-) + docs_urlpatterns
+    url(r'^google79eabba6bf6fd6d3\.html$', lambda req: HttpResponse('google-site-verification: google79eabba6bf6fd6d3.html')),
+)
