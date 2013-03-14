1a94cedae7c9eacf5f755ec1bd37f9c3544576ca
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-03-14 14:28:00 +0100

    Remove hardcoded version numbers from the download page.

--- releases/urls.py
+++ releases/urls.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
 
 from .views import index, redirect
 
 urlpatterns = patterns('',
-    (r'^$', index),
-    (r'^([0-9a-z_.-]+)/(tarball|checksum|egg)/$', redirect),
+    url(r'^$', index, name='download'),
+    url(r'^([0-9a-z_.-]+)/(tarball|checksum|egg)/$', redirect, name='download-redirect'),
 )
