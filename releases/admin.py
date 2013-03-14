e40502fe252e19200e6c95af13312926a9f04460
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-03-14 13:21:41 +0100

    Store each component of the version number separately.
    Improve the admin.
    
    Work around https://code.djangoproject.com/ticket/20049.

--- releases/admin.py
+++ releases/admin.py
@@ -6,7 +6,17 @@ from .models import Release
 
 
 class ReleaseAdmin(admin.ModelAdmin):
-    list_display = ('version', 'date')
-    list_editable = ('date',)
+    list_display = ('version', 'date', 'major', 'minor', 'micro', 'show_status', 'iteration')
+    ordering = ('-major', '-minor', '-micro', '-status', '-iteration')
+
+    def show_status(self, obj):
+        return obj.get_status_display()
+    show_status.admin_order_field = 'status'
+    show_status.short_description = 'status'
+
+    # Hack -- disable logging because it crashes on the non-integer pk
+    def log_addition(self, request, object): pass
+    def log_change(self, request, object): pass
+    def log_deletion(self, request, object): pass
 
 admin.site.register(Release, ReleaseAdmin)
