django/djangoproject.com

commit 957cb2dc9e3e5e6d5ab0013e1555202bf460158e

Replace direct_to_template by TemplateView.

django_www/urls.py
@@ -8,6 +8,8 @@ from django.contrib.comments.models import Comment
 from django.contrib.flatpages.views import flatpage
 from django.contrib.sitemaps import views as sitemap_views
 from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
 from django.views.generic.simple import redirect_to
 
 from accounts import views as account_views
@@ -31,7 +33,7 @@ handler500 = 'django_website.views.server_error'
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'homepage.html'}, name="homepage"),
+    url(r'^$', TemplateView.as_view(template_name='homepage.html'), name="homepage"),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comments/$', 'django.views.generic.list_detail.object_list', comments_info_dict),
@@ -52,7 +54,7 @@ urlpatterns = patterns('',
 
     # PayPal insists on POSTing to the "thank you" page which means we can't
     # just use a flatpage for it.
-    url(r'^foundation/donate/thanks/$', 'django_website.views.donate_thanks'),
+    url(r'^foundation/donate/thanks/$', csrf_exempt(TemplateView.as_view(template_name='donate_thanks.html'))),
 
     # django-push
     url(r'^subscriber/', include('django_push.subscriber.urls')),

contact/urls.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from django.conf.urls import patterns, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from contact_form.views import contact_form
 
@@ -19,10 +19,7 @@ urlpatterns = patterns('',
     ),
     url(
         regex=r'^sent/',
-        view=direct_to_template,
-        kwargs=dict(
-            template='contact/sent.html',
-        ),
+        view=TemplateView.as_view(template_name='contact/sent.html'),
         name='contact_form_sent',
     )
 )

django_website/views.py
@@ -7,20 +7,16 @@ from django.contrib.comments.models import Comment
 from django.contrib.sitemaps import views as sitemap_views
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-from django.views.decorators.csrf import csrf_exempt, requires_csrf_token
+from django.views.decorators.csrf import requires_csrf_token
 from django.views.generic import list_detail
-from django.views.generic.simple import direct_to_template
 
 from .sitemaps import FlatPageSitemap, WeblogSitemap
-
-def homepage(request):
-    return direct_to_template(request, 'homepage.html')
 
 @cache_page(60*60*6)
 def sitemap(request):
     return sitemap_views.sitemap(request, sitemaps={
         'weblog': WeblogSitemap,
-        'flatpages': FlatPageSitemap,    
+        'flatpages': FlatPageSitemap,
     })
 
 def comments(request):
@@ -29,11 +25,6 @@ def comments(request):
         queryset = Comment.objects.filter(is_public=True).order_by('-submit_date'),
         paginate_by = 30,
     )
-
-@csrf_exempt
-def donate_thanks(request):
-    return direct_to_template(request, 'donate_thanks.html')
-
 
 @requires_csrf_token
 def server_error(request, template_name='500.html'):

commit e37f80afd667ef5c2f980643ce1797f3d1770eed

Fix bug introduced in 957cb2dc.
Thanks Baptiste Mispelon.

django_www/urls.py
@@ -42,7 +42,7 @@ urlpatterns = patterns('',
 
     # PayPal insists on POSTing to the "thank you" page which means we can't
     # just use a flatpage for it.
-    url(r'^foundation/donate/thanks/$', csrf_exempt(TemplateView.as_view(template_name='donate_thanks.html'))),
+    url(r'^foundation/donate/thanks/$', csrf_exempt(lambda req: render(req, 'donate_thanks.html'))),
 
     # django-push
     url(r'^subscriber/', include('django_push.subscriber.urls')),