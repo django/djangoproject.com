ea22b2709f2517c8dd28082a3c9f468ec5e0e068
Baptiste Mispelon <bmispelon@gmail.com>
2013-05-21 19:21:13 +0200

    Use upstream django-registration.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,18 +4,14 @@ from django.conf.urls import patterns, url, include
 from django.contrib.auth import views as auth_views
 
 from registration.forms import RegistrationFormUniqueEmail
-from registration import views as registration_views
+from registration.backends.default.views import RegistrationView
 
 from . import views as account_views
 
 urlpatterns = patterns('',
     url(
         r'^register/$',
-        registration_views.register,
-        {
-            'backend': 'registration.backends.default.DefaultBackend',
-            'form_class': RegistrationFormUniqueEmail,
-        },
+        RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),
         name='registration_register',
     ),
     url(
