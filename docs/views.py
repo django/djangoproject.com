89b5cc54cbd1ad4ea512428197d1b788bfc3547a
Jannis Leidel <jannis@leidel.info>
2013-06-20 17:34:32 +0200

    Dog-fooding staticfiles' static template tag.

--- docs/views.py
+++ docs/views.py
@@ -60,10 +60,11 @@ def document(request, lang, version, url):
         'env': json.load(open(docroot.child('globalcontext.json'), 'rb')),
         'lang': lang,
         'version': version,
+        'zipfile_path': 'docs/django-docs-%s-%s.zip' % (version, lang),
         'rtd_version': rtd_version,
         'docurl': url,
         'update_date': datetime.datetime.fromtimestamp(docroot.child('last_build').mtime()),
-        'home': urlresolvers.reverse('document-index', kwargs={'lang':lang, 'version':version}),
+        'home': urlresolvers.reverse('document-index', kwargs={'lang': lang, 'version': version}),
         'redirect_from': request.GET.get('from', None),
     }))
 
