05000184adfb8edae8e5b93a020f840a11fba34c
Jacob Kaplan-Moss <jacob@jacobian.org>
2013-09-07 12:12:40 -0500

    Updated django-contact-form to 1.0.

--- contact/urls.py
+++ contact/urls.py
@@ -3,10 +3,9 @@ from __future__ import absolute_import
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-from . import views
+from .views import ContactFoundation
 
 urlpatterns = patterns('',
-    url(r'^foundation/$', views.contact_foundation, name='contact_foundation'),
+    url(r'^foundation/$', ContactFoundation.as_view(), name='contact_foundation'),
     url(r'^sent/$', TemplateView.as_view(template_name='contact/sent.html'), name='contact_form_sent'),
-    url(r'^code-of-conduct/$', views.contact_coc, name='contact_coc'),
 )
--- contact/views.py
+++ contact/views.py
@@ -1,21 +1,10 @@
-from django.contrib import messages
-
 from django.core import urlresolvers
-from django.http import HttpResponseRedirect
-from contact_form.views import contact_form
-from .forms import FoundationContactForm, CoCFeedbackForm
+from contact_form.views import ContactFormView
+from .forms import FoundationContactForm
 
-def contact_foundation(request):
-    return contact_form(request,
-        form_class = FoundationContactForm,
-        template_name = 'contact/foundation.html',
-        success_url = urlresolvers.reverse('contact_form_sent'))
+class ContactFoundation(ContactFormView):
+    form_class = FoundationContactForm
+    template_name = 'contact/foundation.html'
 
-def contact_coc(request):
-    resp = contact_form(request,
-        form_class = CoCFeedbackForm,
-        template_name = 'contact/coc.html',
-        success_url = urlresolvers.reverse('code_of_conduct'))
-    if isinstance(resp, HttpResponseRedirect):
-        messages.success(request, "Thanks for your feedback! If you provided an email address, we'll get back to you.")
-    return resp
+    def get_success_url(self):
+        return urlresolvers.reverse('contact_form_sent')
