399a1960fc64d483025ce7cb3b7d9f36616f6e20
Baptiste Mispelon <bmispelon@gmail.com>
2013-11-08 18:10:56 +0100

    Improved the documentation search form and view.
    The search form was designed so that, in the absence of a release ID,
    it would yield search results corresponding to the current stable version.
    
    However, there was a bug in the displaying of the form in that
    the current release was not selected in the widget.
    
    Another issue is that it was possible to trigger a 500 error by
    passing an inexistant release ID to the page.
    
    This commit fixes both those issues and a 404 is now raised when
    passing an invalid release ID to the page.

--- docs/forms.py
+++ docs/forms.py
@@ -3,32 +3,43 @@ from django import forms
 from .models import DocumentRelease
 
 
-class DocSearchForm(haystack.forms.SearchForm):
-
-    def __init__(self, data=None, **kwargs):
-        if data and 'release' in data:
-            self.initial_rel = DocumentRelease.objects.get(pk=data['release'])
-        else:
-            self.initial_rel = kwargs.pop('release', DocumentRelease.objects.current())
-        super(DocSearchForm, self).__init__(data=data, **kwargs)
-        self.fields['q'].widget = SearchInput()
-        self.fields['release'] = DocumentReleaseChoiceField(
-            queryset = DocumentRelease.objects.filter(lang=self.initial_rel.lang).order_by('version'),
-            initial = self.initial_rel,
-            empty_label = None,
-            required = False,
-        )
-
-    def search(self):
-        sqs = super(DocSearchForm, self).search()
-        if self.is_valid():
-            rel = self.cleaned_data['release'] or DocumentRelease.objects.current()
-            sqs = sqs.filter(lang=rel.lang, version=rel.version)
-        return sqs
-
 class DocumentReleaseChoiceField(forms.ModelChoiceField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('required', False)
+        kwargs.setdefault('empty_label', None)
+        kwargs.setdefault('queryset', DocumentRelease.objects.order_by('version'))
+        super(DocumentReleaseChoiceField, self).__init__(*args, **kwargs)
+
     def label_from_instance(self, obj):
         return obj.human_version
 
-class SearchInput(forms.TextInput):
-    input_type = 'search'
+    def bound_data(self, data, initial):
+        """
+        If no data is given, return the initial data.
+        This allows for a default release to always be selected, even if none
+        was provided in the URL.
+        """
+        return data or initial
+
+
+class DocSearchForm(haystack.forms.SearchForm):
+    release = DocumentReleaseChoiceField()
+
+    def __init__(self, data=None, **kwargs):
+        self.default_release = kwargs.pop('default_release')
+        super(DocSearchForm, self).__init__(data=data, **kwargs)
+        self.fields['q'].widget = forms.TextInput(attrs={'type': 'search'})
+        self.fields['release'].queryset = self.fields['release'].queryset.filter(lang=self.default_release.lang)
+        self.fields['release'].initial = self.default_release
+
+    def search(self):
+        results = super(DocSearchForm, self).search()
+        assert self.cleaned_data  # SearchForm.search() calls is_valid()
+        release = self.cleaned_data['release']
+        results = results.filter(lang=release.lang, version=release.version)
+        return results
+
+    def clean_release(self):
+        """If no release is provided we fall back to the default release."""
+        release = self.cleaned_data.get('release')
+        return release if release else self.default_release
