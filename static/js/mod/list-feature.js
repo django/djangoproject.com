123bc6212ffdafec5e95aa534645f4b16d953460
Ola Sitarska <ola@sitarska.com>
2014-11-15 17:47:49 +0100

    Removed old static files, added new static files after redesign

---
+++ static/js/mod/list-feature.js
@@ -0,0 +1,23 @@
+define([
+	'jquery.inview' //required inview plugin
+], function( $ ) {
+
+	var FeatureList = function(list) {
+		this.list = $(list);
+		this.init();
+	};
+	
+	FeatureList.prototype = {
+		init: function(){
+			this.icons = this.list.find('dt i'); //go get icons
+			this.icons.bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+				if (isInView && visiblePartY != 'top' && visiblePartY != 'bottom') { // element completely visible
+					$(this).addClass('inview'); //new class
+				} 
+			});
+		}
+	};
+
+	// Export a single instance of our module:
+	return new FeatureList('.list-features');
+});
