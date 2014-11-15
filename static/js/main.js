88bcfc328b477b53122bd76b9683de3de3e13054
Ola Sitarska <ola@sitarska.com>
2014-11-15 17:47:49 +0100

    Removed old static files, added new static files after redesign

---
+++ static/js/main.js
@@ -0,0 +1,58 @@
+// Require.js Module Loader - http://requirejs.org
+
+requirejs.config({
+	paths: {
+		"jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+		// "jquery": "lib/jquery",  --  local version of jquery if desired.
+		"jquery.inview": "lib/jquery.inview"
+	}
+});
+
+define(function(){
+
+	var mods = [
+		'mod/mobile-menu' //require mobile menu automatically
+	]; 
+
+	//detect Class function
+	function hasClass( className ) { 
+		if (!document.getElementsByClassName) { //class name function in old IE
+			document.getElementsByClassName = function(search) {
+				var d = document, elements, pattern, i, results = [];
+				if (d.querySelectorAll) { // IE8
+					return d.querySelectorAll("." + search);
+				}
+				if (d.evaluate) { // IE6, IE7
+					pattern = ".//*[contains(concat(' ', @class, ' '), ' " + search + " ')]";
+					elements = d.evaluate(pattern, d, null, 0, null);
+					while ((i = elements.iterateNext())) {
+						results.push(i);
+					}
+				} else {
+					elements = d.getElementsByTagName("*");
+					pattern = new RegExp("(^|\\s)" + search + "(\\s|$)");
+					for (i = 0; i < elements.length; i++) {
+						if ( pattern.test(elements[i].className) ) {
+							results.push(elements[i]);
+						}
+					}
+				}
+				return results;
+			}
+		}
+		return !!document.getElementsByClassName( className ).length; //return a boolean
+	}
+
+	//feature list
+	if (hasClass('list-features')) { 
+		mods.push('mod/list-feature');
+	}
+
+	//collapsing list
+	if (hasClass('list-collapsing')) { 
+		mods.push('mod/list-collapsing');
+	}
+
+	require(mods);
+
+});
