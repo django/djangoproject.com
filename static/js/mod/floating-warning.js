cf7f6ec435f6c7a06f9ae6f62e82e5faedd945b0
Jannis Leidel <jannis@leidel.info>
2014-12-19 01:40:40 +0100

    Fix #220 - Again, this time by using hashchange event.

--- static/js/mod/floating-warning.js
+++ static/js/mod/floating-warning.js
@@ -15,39 +15,28 @@ define([
 				// to force correct spacing of header.
 				self.warning.clone().prependTo('body').css('position', 'relative');
 
-				// Here we are doing something really annoying, we catch clicks on
-				// internal anchors and scroll to the right place of the page.
-				// This is only needed because we want to show the developement version
-				// warning on top.
-				// First handle the case in which someone clicks on a link
-				 $('a[href*=#]').click(function () {
-					// check to see if this is an internal link, sigh.
-					if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-						location.hostname == this.hostname) {
-						// get the link target, use the weird id= query since it escapes dots
-						var target = $("[id='" + this.hash.slice(1) + "']");
-						// calculate the offset
-						var targetOffset = target.offset().top - self.warning.height() - 20;
-						// scroll to the place, there is probably a better way
-						setTimeout(function () {
-						  $('html,body').scrollTop(targetOffset);
-						}, 50);
-					}
-				});
-				// this is the janky thing, there is probably a better way
 				setTimeout(function () {
-					// is there a hash in the current window's location?
-					if (window.location.hash) {
-						// again, get the target
-						var target = $("[id='" + window.location.hash.slice(1) + "']");
-						// calculate the offset
-						var targetOffset = target.offset().top - self.warning.height() - 20;
-						// scroll to the place
-						$('html,body').scrollTop(targetOffset);
-					}
-				// use a delay that should work on all modern computers. should, not will
-				}, 50);
+					self.scroll(window.location.hash, self.warning);
+				}, 50); // use a delay that should work on all modern computers. should, not will
+
+				// use something nicer
+				$(window).on('hashchange', function() {
+					self.scroll(window.location.hash, self.warning);
+				});
 			});
+		},
+		scroll: function(hash, warning) {
+			// is there a hash in the current window's location?
+			if (hash) {
+				// again, get the target
+				var target = $("[id='" + hash.slice(1) + "']");
+				if (target.length) {
+					// calculate the offset
+					var targetOffset = target.offset().top - warning.height() - 20;
+					// scroll to the place
+					$('html,body').scrollTop(targetOffset);
+				}
+			}
 		}
 	};
 
