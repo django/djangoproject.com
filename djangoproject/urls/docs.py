43c94efb54889e37852d9814d14d4f475a9ac8f6
Jannis Leidel <jannis@leidel.info>
2014-12-30 20:47:37 +0100

    Added a comment about the legacy URL patterns existing on the docs page, too.

--- djangoproject/urls/docs.py
+++ djangoproject/urls/docs.py
@@ -11,5 +11,6 @@ sitemaps = {'docs': DocsSitemap}
 urlpatterns = docs_urlpatterns + [
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
     url(r'^google79eabba6bf6fd6d3\.html$', lambda req: HttpResponse('google-site-verification: google79eabba6bf6fd6d3.html')),
+    # This just exists to make sure we can proof that the error pages work under both hostnames.
     url(r'', include('legacy.urls')),
 ]
