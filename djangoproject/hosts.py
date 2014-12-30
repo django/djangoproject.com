44beabeae6e7d701647e4d679a1ccc6eed47249d
Jannis Leidel <jannis@leidel.info>
2014-12-30 21:06:12 +0100

    Made host patterns a list to follow Django's lead.

--- djangoproject/hosts.py
+++ djangoproject/hosts.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django_hosts import patterns, host
+from django_hosts import host
 
-host_patterns = patterns('',
+host_patterns = [
     host(r'www', settings.ROOT_URLCONF, name='www'),
     host(r'docs', 'djangoproject.urls.docs', name='docs'),
-)
+]
