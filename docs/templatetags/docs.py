c863b7fa8ae1b747df8468fdf8f4e8e0b14b81f1
Jannis Leidel <jannis@leidel.info>
2015-01-10 22:24:31 +0100

    Turned get_all_doc_versions into an assignment_tag

--- docs/templatetags/docs.py
+++ docs/templatetags/docs.py
@@ -17,47 +17,27 @@ def search_form(context, search_form_id='sidebar_search'):
     }
 
 
-@register.tag
-def get_all_doc_versions(parser, token):
+@register.assignment_tag(takes_context=True)
+def get_all_doc_versions(context, url=None):
     """
     Get a list of all versions of this document to link to.
 
-    Usage: {% get_all_doc_versions <docurl> as "varname" %}
+    Usage: {% get_all_doc_versions <url> as "varname" %}
     """
-    return AllDocVersionsTag.handle(parser, token)
+    lang = context.get('lang', 'en')
+    if url is None:
+        versions = DocumentRelease.objects.filter(lang=lang).order_by('version')
+        return versions.value_list('version', flat=True)
 
+    versions = []
 
-class AllDocVersionsTag(template.Node):
-    @classmethod
-    def handle(cls, parser, token):
-        try:
-            tagname, docurl, as_, asvar = token.split_contents()
-        except ValueError:
-            raise template.TemplateSyntaxError("Usage: {% get_all_doc_versions <docurl> as <varname> %}")
-        return cls(docurl, asvar)
+    # Look for each version of the docs.
+    for release in DocumentRelease.objects.filter(lang=lang):
+        version_root = get_doc_root(release.lang, release.version)
+        if version_root.exists():
+            doc_path = get_doc_path(version_root, url)
+            if doc_path:
+                versions.append(release.version)
 
-    def __init__(self, docurl, asvar):
-        self.docurl = template.Variable(docurl)
-        self.asvar = asvar
-
-    def render(self, context):
-        try:
-            url = self.docurl.resolve(context)
-        except template.VariableDoesNotExist:
-            return ''
-
-        versions = []
-        lang = context.get('lang', 'en')
-
-        # Look for each version of the docs.
-        for release in DocumentRelease.objects.filter(lang=lang):
-            version_root = get_doc_root(release.lang, release.version)
-            if version_root.exists():
-                doc_path = get_doc_path(version_root, url)
-                if doc_path:
-                    versions.append(release.version)
-
-        # Save the versions into the context
-        context[self.asvar] = sorted(versions)
-
-        return ''
+    # Save the versions into the context
+    return sorted(versions)
