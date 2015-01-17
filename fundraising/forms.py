4ad849c96b776a696a77df7929543b9b095105a1
Aleksandra Sendecka <asendecka@hauru.eu>
2015-01-17 21:17:54 +0100

    Fundraising files: cleanup, better code style.

--- fundraising/forms.py
+++ fundraising/forms.py
@@ -4,14 +4,37 @@ from .models import DjangoHero
 
 
 class AddDjangoHeroForm(forms.ModelForm):
-    email = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'required', 'placeholder': 'E-mail address'}),
-                            help_text='We ask for email because we need to match your donation with information from this form.')
-    name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'required', 'placeholder': 'Your name or name of your organization'}))
-    url = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Should we link your name to somewhere? Tell us where'}))
-    logo = forms.FileField(required=False, help_text='If you donated at least $200, you can submit your logo and we will display it too.')
-    is_visible = forms.BooleanField(required=False, label='Yes, I want this informations to be displayed on the Fundraising page.')
-    is_subscribed = forms.BooleanField(required=False, label='Django Software Foundation can occasionally inform me about fundraising.')
-    is_amount_displayed = forms.BooleanField(required=False, label='Yes, information about the amount of my donation can be public.')
+    email = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'required', 'placeholder': 'E-mail address'}),
+        help_text='We ask for email because we need to match your donation with information from this form.'
+    )
+    name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'required', 'placeholder': 'Your name or name of your organization'})
+    )
+    url = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Should we link your name to somewhere? Tell us where'})
+    )
+    logo = forms.FileField(
+        required=False,
+        help_text='If you donated at least $200, you can submit your logo and we will display it too.'
+    )
+    is_visible = forms.BooleanField(
+        required=False,
+        label='Yes, I want this informations to be displayed on the Fundraising page.'
+    )
+    is_subscribed = forms.BooleanField(
+        required=False,
+        label='Django Software Foundation can occasionally inform me about fundraising.'
+    )
+    is_amount_displayed = forms.BooleanField(
+        required=False,
+        label='Yes, information about the amount of my donation can be public.'
+    )
+
+    THANK_YOU_CHECKBOX_FIELDS = ['is_visible', 'is_subscribed', 'is_amount_displayed']
 
     class Meta:
         model = DjangoHero
