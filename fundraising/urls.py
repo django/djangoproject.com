7d4bb9e9cbb21ebf6200939829437243f240e1d5
Ola Sitarska <ola@sitarska.com>
2015-01-17 18:09:57 +0000

    Added view, form and styles for displaying the form for Django Heros. Refs #295

--- fundraising/urls.py
+++ fundraising/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from . import views
+
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='fundraising/index.html'), name='fundraising'),
+    url(r'^$', views.fundraising_index, name='fundraising'),
+    url(r'^thank-you/$', views.fundraising_thank_you, name='fundraising-thank-you'),
 ]
--- fundraising/views.py
+++ fundraising/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.core import urlresolvers
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import AddDjangoHeroForm
+
+def fundraising_index(request):
+    return render(request, 'fundraising/index.html', {})
+
+def fundraising_thank_you(request):
+    form = AddDjangoHeroForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Thank you! You're a Hero.")
+        return redirect(reverse('fundraising'))
+
+    return render(request, 'fundraising/thank-you.html', {'form': form})
