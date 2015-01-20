93d7d7b8483f54d57bb9784adcc6667bb4a27e7a
Ola Sitarska <ola@sitarska.com>
2015-01-20 22:31:17 +0000

    Added donation snippet to sidebars. Refs #298

--- fundraising/templatetags/fundraising_extras.py
+++ fundraising/templatetags/fundraising_extras.py
@@ -1,8 +1,10 @@
 from decimal import Decimal
 
 from django import template
+from django.db import models
 from django.template.defaultfilters import floatformat
 
+from fundraising.models import DjangoHero
 
 register = template.Library()
 
@@ -16,3 +18,14 @@ def as_percentage(part, total):
         return floatformat((part / total) * Decimal("100.0"))
     except ZeroDivisionError:
         return "0.00"
+
+
+@register.inclusion_tag('fundraising/donation_snippet.html')
+def donation_snippet():
+    try:
+        donation = DjangoHero.objects.filter(is_visible=True).order_by('?')[:1]
+        donation = donation.annotate(donated_amount=models.Sum('donation__amount')).get()
+    except DjangoHero.DoesNotExist:
+        donation = None
+
+    return {'donation': donation}
