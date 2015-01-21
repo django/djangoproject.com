194e00f5a68545f0d9095d094d4db6fee1565c24
Baptiste Mispelon <bmispelon@gmail.com>
2015-01-21 23:58:41 +0100

    Added a verbose_name and verbose_name_plural to DjangoHero
    This fixes bad pluralization in the admin.

---
+++ fundraising/migrations/0011_auto_20150121_1710.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fundraising', '0010_auto_20150121_0818'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='djangohero',
+            options={'verbose_name': 'Django hero', 'verbose_name_plural': 'Django heroes'},
+        ),
+    ]
