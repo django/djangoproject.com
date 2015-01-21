bde1941eff613b9a61640ae2ae380795a6a74716
Ola Sitarska <ola@sitarska.com>
2015-01-21 22:05:55 +0000

    Added tests making sure that donation forms are rendered properly.

--- fundraising/tests.py
+++ fundraising/tests.py
@@ -2,6 +2,9 @@ from datetime import date, timedelta
 from decimal import Decimal
 from operator import attrgetter
 
+from django import forms
+from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from .models import DjangoHero, Donation
@@ -12,6 +15,40 @@ class TestIndex(TestCase):
         DjangoHero.objects.create()
         response = self.client.get('/fundraising/')
         self.assertEqual(response.context['total_donors'], 1)
+
+    def test_render_donate_form_with_amount(self):
+        response = self.client.get(reverse('fundraising:donate'), {'amount': 50})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['fixed_amount'], '50')
+        self.assertEqual(response.context['publishable_key'], settings.STRIPE_PUBLISHABLE_KEY)
+
+        # Checking if amount field is hidden
+        self.assertEqual(response.context['form'].fields['amount'].widget.__class__, forms.HiddenInput)
+        # Checking if campaign field is empty
+        self.assertEqual(response.context['form'].initial['campaign'], None)
+
+    def test_render_donate_form_without_amount(self):
+        response = self.client.get(reverse('fundraising:donate'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['fixed_amount'], None)
+        self.assertEqual(response.context['publishable_key'], settings.STRIPE_PUBLISHABLE_KEY)
+
+        # Checking if amount field is visible
+        self.assertEqual(response.context['form'].fields['amount'].widget.__class__, forms.TextInput)
+        # Checking if campaign field is empty
+        self.assertEqual(response.context['form'].initial['campaign'], None)
+
+    def test_render_donate_form_with_campaign(self):
+        campaign = 'sample'
+        response = self.client.get(reverse('fundraising:donate'), {'amount': 100, 'campaign':campaign})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['fixed_amount'], '100')
+        self.assertEqual(response.context['publishable_key'], settings.STRIPE_PUBLISHABLE_KEY)
+
+        # Checking if amount field is hidden
+        self.assertEqual(response.context['form'].fields['amount'].widget.__class__, forms.HiddenInput)
+        # Checking if campaign field is same as campaign
+        self.assertEqual(response.context['form'].initial['campaign'], campaign)
 
 
 class TestDjangoHero(TestCase):
