5ec5c4b322ee19be7e4706bfda95dcc7b2376dfc
Jannis Leidel <jannis@leidel.info>
2015-01-21 16:42:24 +0100

    Use small letters for secret names.

--- djangoproject/settings/common.py
+++ djangoproject/settings/common.py
@@ -228,5 +228,5 @@ SUPERFEEDR_CREDS = SECRETS.get('superfeedr_creds')
 # Stripe settings
 
 # only testing keys as fallback values here please!
-STRIPE_SECRET_KEY = SECRETS.get('STRIPE_SECRET_KEY', 'sk_test_x6zP4wd7Z5jcvDOJbbHZlHHt')
-STRIPE_PUBLISHABLE_KEY = SECRETS.get('STRIPE_PUBLISHABLE_KEY', 'pk_test_TyB5jcROwK8mlCNrn3dCwW7l')
+STRIPE_SECRET_KEY = SECRETS.get('stripe_secret_key', 'sk_test_x6zP4wd7Z5jcvDOJbbHZlHHt')
+STRIPE_PUBLISHABLE_KEY = SECRETS.get('stripe_publishable_key', 'pk_test_TyB5jcROwK8mlCNrn3dCwW7l')
