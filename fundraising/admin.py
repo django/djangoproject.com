c30e35cf045bbaeeee8f94c32fb11ee5df6fb7d8
Jannis Leidel <jannis@leidel.info>
2015-01-25 15:15:35 +0100

    Improved DjangoHero and Donation admin UI.

--- fundraising/admin.py
+++ fundraising/admin.py
@@ -4,12 +4,18 @@ from .models import DjangoHero, Donation, Testimonial
 
 @admin.register(DjangoHero)
 class DjangoHeroAdmin(admin.ModelAdmin):
-    list_filter = ['approved']
+    list_filter = ['approved', 'created', 'modified',
+                   'is_visible', 'is_subscribed', 'is_amount_displayed']
+    list_display = ['id', 'name', 'email', 'created', 'modified', 'approved']
+    list_editable = ['approved']
 
 
 @admin.register(Donation)
 class Donation(admin.ModelAdmin):
     raw_id_fields = ['donor']
+    list_display = ['id', 'amount', 'donor', 'stripe_charge_id',
+                    'created', 'modified', 'campaign_name']
+    list_filter = ['campaign_name', 'created', 'modified']
 
 
 @admin.register(Testimonial)
