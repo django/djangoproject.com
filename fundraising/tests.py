33f24121b00f2d836862ff03a3e464a260bc7f1b
Rolf Erik Lekang <me@rolflekang.com>
2015-01-28 14:27:27 +0100

    Create folder in thumbnail test

--- fundraising/tests.py
+++ fundraising/tests.py
@@ -235,14 +235,18 @@ class TestDjangoHero(TestCase):
                 )
 
     def test_thumbnail(self):
+        try:
+            os.makedirs(os.path.join(settings.MEDIA_ROOT, 'fundraising/logos/'))
+        except OSError:  # directory may already exist
+            pass
         image_path = os.path.join(settings.MEDIA_ROOT, 'fundraising/logos/test_logo.jpg')
         image = Image.new('L', (500, 500))
         image.save(image_path)
         self.h1.logo = image_path
         self.h1.save()
         thumbnail = self.h1.thumbnail
-        self.assertEqual(thumbnail.x, 340)
-        self.assertEqual(thumbnail.y, 340)
+        self.assertEqual(thumbnail.x, 170)
+        self.assertEqual(thumbnail.y, 170)
         os.remove(image_path)
         self.assertTrue(
             os.path.exists(
