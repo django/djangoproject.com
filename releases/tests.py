c1d4c9699856997c867646e7280cf7a62c5316c7
Tim Graham <timograham@gmail.com>
2015-04-02 10:07:45 -0400

    Added a "Supported Versions" table to the downloads page.

--- releases/tests.py
+++ releases/tests.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from .models import Release, create_releases_up_to_1_5
+from .templatetags.release_notes import get_latest_micro_release
 
 
 class LegacyURLsTests(TestCase):
@@ -42,3 +43,12 @@ class TestDownloadsView(TestCase):
         self.assertEqual(response.context['lts_version'], '1.4')
         # No longer supported versions
         self.assertEqual(response.context['earlier_versions'], ['1.6'])
+
+
+class TestTemplateTags(TestCase):
+    def test_get_latest_micro_release(self):
+        Release.objects.create(major=1, minor=8, micro=0, is_lts=True, version='1.8')
+        Release.objects.create(major=1, minor=8, micro=1, is_lts=True, version='1.8.1')
+
+        self.assertEqual(get_latest_micro_release('1.8'), '1.8.1')
+        self.assertEqual(get_latest_micro_release('1.4'), None)
