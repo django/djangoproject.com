59abff2df6e68b7e8a61dde3607a9a2d38d252e7
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2015-06-04 12:47:47 +0100

    Admin for Payment changes.
    Refs #386.

--- fundraising/admin.py
+++ fundraising/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.db.models import Sum
 from sorl.thumbnail.admin import AdminImageMixin
 
-from .models import Campaign, DjangoHero, Donation, Testimonial
+from .models import Campaign, DjangoHero, Donation, Payment, Testimonial
 
 
 @admin.register(DjangoHero)
@@ -12,13 +13,28 @@ class DjangoHeroAdmin(AdminImageMixin, admin.ModelAdmin):
     ordering = ['-created']
 
 
+class PaymentInline(admin.TabularInline):
+    model = Payment
+
+
 @admin.register(Donation)
 class Donation(admin.ModelAdmin):
     raw_id_fields = ['donor']
-    list_display = ['id', 'amount', 'donor', 'stripe_charge_id',
-                    'created', 'modified']
+    list_display = ['id', 'amount', 'donor', 'created', 'modified']
     list_filter = ['created', 'modified']
     ordering = ['-created']
+    inlines = [PaymentInline]
+
+    def get_queryset(self, request):
+        """Annotate the sum of related payments to every donation."""
+        qs = super(Donation, self).get_queryset(request)
+        return qs.annotate(amount=Sum('payment__amount'))
+
+    def amount(self, obj):
+        # Since amount is an annotated field, it is not recognized as a property
+        # of the model for list_display so we need an actual method that
+        # references it.
+        return obj.amount
 
 
 @admin.register(Testimonial)
