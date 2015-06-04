42d9eeefb31567257d2594d4d413ea5588435f45
Ola <ola@sitarska.com>
2015-06-04 11:38:22 +0100

    Fixed inconsisten urls

--- fundraising/urls.py
+++ fundraising/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^campaign/(?P<slug>[\w-]+)/$', views.campaign, name='campaign'),
     url(r'^donate/$', views.donate, name='donate'),
     url(r'^thank-you/(?P<donation>[\w]+)/$', views.thank_you, name='thank-you'),
-    url(r'^manage_donations/(?P<hero>[\w]+)/$', views.manage_donations, name='manage-donations'),
-    url(r'^manage_donations/(?P<hero>[\w]+)/cancel/(?P<donation>[\w]+)$', views.cancel_donation, name='cancel-donation'),
+    url(r'^manage-donations/(?P<hero>[\w]+)/$', views.manage_donations, name='manage-donations'),
+    url(r'^manage-donations/(?P<hero>[\w]+)/cancel/(?P<donation>[\w]+)$', views.cancel_donation, name='cancel-donation'),
 ]
