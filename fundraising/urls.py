629fd0769d76683529c83b7aa870f9948c2c01e2
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2015-06-05 10:57:56 +0100

    Handle payment webhook calls.
    Stripe will send an "invoice.payment_succeeded" event when a regular
    payment is made as part of a subscription. This change adds a generic
    webhook handler and specific code to create a Payment in response to
    this event.

--- fundraising/urls.py
+++ fundraising/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^thank-you/(?P<donation>[\w]+)/$', views.thank_you, name='thank-you'),
     url(r'^manage-donations/(?P<hero>[\w]+)/$', views.manage_donations, name='manage-donations'),
     url(r'^manage-donations/(?P<hero>[\w]+)/cancel/(?P<donation>[\w]+)$', views.cancel_donation, name='cancel-donation'),
+    url(r'^receive_webhook/', views.receive_webhook, name='receive-webhook'),
 ]
