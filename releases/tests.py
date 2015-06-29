9cc3634c6231c9f341e252bd6979d31424acf7c7
Tim Graham <timograham@gmail.com>
2015-06-29 14:15:47 -0400

    Adapted release_notes template tag for simple_tag escaping in Django 1.9.

--- releases/tests.py
+++ releases/tests.py
@@ -1,9 +1,10 @@
 from django.contrib.redirects.models import Redirect
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.utils.safestring import SafeString
 
 from .models import Release, create_releases_up_to_1_5
-from .templatetags.release_notes import get_latest_micro_release
+from .templatetags.release_notes import get_latest_micro_release, release_notes
 
 
 class LegacyURLsTests(TestCase):
@@ -50,3 +51,19 @@ class TestTemplateTags(TestCase):
 
         self.assertEqual(get_latest_micro_release('1.8'), '1.8.1')
         self.assertEqual(get_latest_micro_release('1.4'), None)
+
+    def test_release_notes(self):
+        Release.objects.create(major=1, minor=8, micro=0, is_lts=True, version='1.8')
+
+        output = release_notes('1.8')
+        self.assertIsInstance(output, SafeString)
+        self.assertEqual(
+            output,
+            '<a href="http://docs.djangoproject.dev:8000/en/1.8/releases/1.8/">'
+            'Online documentation</a>'
+        )
+        self.assertEqual(
+            release_notes('1.8', show_version=True),
+            '<a href="http://docs.djangoproject.dev:8000/en/1.8/releases/1.8/">'
+            '1.8 release notes</a>'
+        )
