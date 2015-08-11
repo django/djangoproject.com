47e5cbbe7f204680917fd0bf2582b140e37fe2bb
Jannis Leidel <jannis@leidel.info>
2015-08-11 10:58:54 +0200

    Added lazy loading for Django Hero images on fundraising page.

--- djangoproject/static/js/mod/alternative-resolutions.js
+++
@@ -1,8 +0,0 @@
-define(['jquery', 'modernizr-retina-test'], function ($){
-    if (Modernizr.hires) {
-        $('img[data-src-2x]').each(function() {
-            $this = $(this);
-            $this.attr('src', $this.attr('data-src-2x'))
-        });
-    }
-});
---
+++ djangoproject/static/js/mod/fundraising-heros.js
@@ -0,0 +1,27 @@
+define([
+	'jquery', //requires jquery
+	'jquery.unveil' // requires unveil.js
+	], function($) {
+		// This will makes the Django hero images only be loaded
+		// when scrolled to.
+
+		// Load images 200 pixels before they become visible,
+		// assumes an image size of 170 px plus some margin
+		var threshold = 200;
+
+		var FundraisingHeros = function(images) {
+			this.images = $(images); // the heros images
+			this.init();
+		};
+
+		FundraisingHeros.prototype = {
+			init: function() {
+				var self = this;
+				$(document).ready(function() {
+					self.images.unveil(threshold);
+				});
+			},
+		};
+		// Export a single instance of our module:
+		return new FundraisingHeros('.hero-logo img');
+});
