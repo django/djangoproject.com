324e3600bdaf20827779c52a1170570c7657e1da
Tim Graham <timograham@gmail.com>
2015-09-28 09:17:24 -0400

    Removed usage of deprecated NoArgsCommand.

--- dashboard/management/commands/update_metrics.py
+++ dashboard/management/commands/update_metrics.py
@@ -1,11 +1,11 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management import BaseCommand
 
 from ...models import Metric
 from ...utils import reset_generation_key
 
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
+class Command(BaseCommand):
+    def handle(self, **options):
         verbose = int(options.get('verbosity', 0))
         for MC in Metric.__subclasses__():
             for metric in MC.objects.all():
