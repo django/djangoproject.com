1b1f006a8f9014379469da3430aa88ddb580a126
Baptiste Mispelon <bmispelon@gmail.com>
2015-11-16 13:30:59 +0100

    Added new management command that combines update_docs and update_index.
    The server will use this because we don't have an easy way
    to run two management commands sequentially.

---
+++ docs/management/commands/update_docs_and_index.py
@@ -0,0 +1,11 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Update the docs then reindex them in elasticsearch.
+    """
+    def handle(self, **options):
+        call_command('update_docs', **options)
+        call_command('update_index', delete=True, **options)
