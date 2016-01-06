3fde4ffd45f65406e4d317267580ac0fb81b4b73
Tim Graham <timograham@gmail.com>
2016-01-05 20:25:55 -0500

    Fixed #606 -- Added expires and invoiced columns to corporate members admin.

--- members/admin.py
+++ members/admin.py
@@ -1,4 +1,8 @@
+from datetime import date, timedelta
+
 from django.contrib import admin
+from django.utils.formats import localize
+from django.utils.html import format_html
 
 from members.models import CorporateMember, DeveloperMember, Invoice
 
@@ -23,6 +27,8 @@ class InvoiceInline(admin.TabularInline):
 class CorporateMemberAdmin(admin.ModelAdmin):
     list_display = [
         'display_name',
+        'membership_expires',
+        '_is_invoiced',
         'contact_email',
         'membership_level',
     ]
@@ -31,3 +37,26 @@ class CorporateMemberAdmin(admin.ModelAdmin):
     ]
     inlines = [InvoiceInline]
     search_fields = ['name']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('invoice_set')
+
+    def membership_expires(self, obj):
+        expiry_date = obj.get_expiry_date()
+        if expiry_date:
+            today = date.today()
+            # Expired.
+            if expiry_date < today:
+                color = 'red'
+            # Expires within 30 days.
+            elif expiry_date < today + timedelta(days=30):
+                color = 'orange'
+            # Expires more than 30 days from today.
+            else:
+                color = 'green'
+            expiry_date = format_html(
+                '<span style="color: {}">{}</span>',
+                color,
+                localize(expiry_date),
+            )
+        return expiry_date
---
+++ members/test_admin.py
@@ -0,0 +1,35 @@
+from datetime import date, timedelta
+
+from django.contrib import admin
+from django.test import TestCase
+
+from .admin import CorporateMemberAdmin
+from .models import CorporateMember
+
+
+class CorporateMemberAdminTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.member = CorporateMember.objects.create(
+            display_name='Corporation',
+            billing_name='foo',
+            billing_email='c@example.com',
+            contact_email='c@example.com',
+            membership_level=2,
+        )
+
+    def test_membership_expires(self):
+        today = date.today()
+        yesterday = date.today() - timedelta(days=1)
+        plus_thirty_one_days = today + timedelta(days=31)
+        modeladmin = CorporateMemberAdmin(CorporateMember, admin.site)
+        self.assertIsNone(modeladmin.membership_expires(self.member))
+        self.member.invoice_set.create(amount=500)
+        self.assertIsNone(modeladmin.membership_expires(self.member))
+        self.member.invoice_set.create(amount=500, expiration_date=yesterday)
+        self.assertIn('red', modeladmin.membership_expires(self.member))
+        self.member.invoice_set.create(amount=500, expiration_date=today)
+        self.assertIn('orange', modeladmin.membership_expires(self.member))
+        self.member.invoice_set.create(amount=500, expiration_date=plus_thirty_one_days)
+        self.assertIn('green', modeladmin.membership_expires(self.member))
