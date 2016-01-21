2c6f279e629b088cebf4d787bd33a8028116d6ef
Tim Graham <timograham@gmail.com>
2016-01-20 20:45:47 -0500

    Removed usage of deprecated assertEquals().

--- svntogit/tests.py
+++ svntogit/tests.py
@@ -6,13 +6,13 @@ class SvnToGitTests(TestCase):
     def test_redirect(self):
         response = self.client.get('/svntogit/1/', follow=False)
         target = 'https://github.com/django/django/commit/d6ded0e91b'
-        self.assertEquals(response.status_code, 301)
-        self.assertEquals(response['Location'], target)
+        self.assertEqual(response.status_code, 301)
+        self.assertEqual(response['Location'], target)
 
     def test_redirect_empty_changeset(self):
         response = self.client.get('/svntogit/7/')
-        self.assertEquals(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
 
     def test_redirect_non_existing_changeset(self):
         response = self.client.get('/svntogit/20000/')
-        self.assertEquals(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
