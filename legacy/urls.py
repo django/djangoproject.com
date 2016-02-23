ad0351748b108cd3680ce6e56a2fc590e8f4352a
Tim Graham <timograham@gmail.com>
2016-02-23 09:15:31 -0500

    Fixed test error views.

--- legacy/urls.py
+++ legacy/urls.py
@@ -1,6 +1,8 @@
 """
 Legacy URLs for documentation pages.
 """
+from functools import partial
+
 from django.conf.urls import url
 from django.views import defaults
 
@@ -11,9 +13,9 @@ urlpatterns = [
     url(r'^rss/comments/$', gone),
     url(r'^documentation', gone),
 
-    url(r'^400/$', defaults.bad_request),
-    url(r'^403/$', defaults.permission_denied),
-    url(r'^404/$', defaults.page_not_found),
+    url(r'^400/$', partial(defaults.bad_request, exception=None)),
+    url(r'^403/$', partial(defaults.permission_denied, exception=None)),
+    url(r'^404/$', partial(defaults.page_not_found, exception=None)),
     url(r'^410/$', gone),
     url(r'^500/$', defaults.server_error),
 ]
