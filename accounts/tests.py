c59dcc975d8d2ed1c99f098853930f61fc11e9c4
Tim Graham <timograham@gmail.com>
2016-08-25 11:05:38 -0400

    Fixed #695 -- Added a login redirect URL.

--- accounts/tests.py
+++ accounts/tests.py
@@ -8,10 +8,15 @@ from django_hosts.resolvers import reverse
 class ViewTests(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(username='a-user')
+        self.credentials = {'username': 'a-user', 'password': 'password'}
+        self.user = User.objects.create_user(**self.credentials)
 
     @mock.patch('accounts.views.get_user_stats')
     def test_user_profile(self, mock_user_stats):
         response = self.client.get(reverse('user_profile', host='www', args=['a-user']))
         self.assertContains(response, 'a-user')
         mock_user_stats.assert_called_once_with(self.user)
+
+    def test_login_redirect(self):
+        response = self.client.post(reverse('login'), self.credentials)
+        self.assertRedirects(response, '/accounts/edit/')
