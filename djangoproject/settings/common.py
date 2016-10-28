4f5c78bd8e2654b7e767db3dc528e87206a36fb1
Tim Graham <timograham@gmail.com>
2016-10-27 20:28:04 -0400

    Removed unused mail_admins logging handler.

--- djangoproject/settings/common.py
+++ djangoproject/settings/common.py
@@ -101,11 +101,6 @@ LOGGING = {
         },
     },
     "handlers": {
-        "mail_admins": {
-            "level": "ERROR",
-            "filters": ['require_debug_false'],
-            "class": "django.utils.log.AdminEmailHandler",
-        },
         "console": {
             "level": "DEBUG",
             "class": "logging.StreamHandler",
@@ -114,7 +109,7 @@ LOGGING = {
     },
     "loggers": {
         "django.request": {
-            "handlers": ["mail_admins"],
+            "handlers": [],
             "level": "ERROR",
             "propagate": False,
         },
