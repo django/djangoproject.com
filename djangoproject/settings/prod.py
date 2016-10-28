d8b0f05c74fd557208d600d516021752eabba16e
Tim Graham <timograham@gmail.com>
2016-10-27 20:28:04 -0400

    Removed unused mail_admins logging handler.

--- djangoproject/settings/prod.py
+++ djangoproject/settings/prod.py
@@ -62,4 +62,3 @@ PUSH_SSL_CALLBACK = True
 if 'sentry_dsn' in SECRETS and not DEBUG:
     INSTALLED_APPS.append('raven.contrib.django.raven_compat')
     RAVEN_CONFIG = {'dsn': SECRETS['sentry_dsn']}
-    LOGGING["loggers"]["django.request"]["handlers"].remove("mail_admins")
