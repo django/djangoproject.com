08fc573b1893f06ec6b16bc91b0053bac208aefd
Tim Graham <timograham@gmail.com>
2016-11-28 11:40:48 -0500

    Removed unused 'using' argument for update_index command.

--- docs/management/commands/update_index.py
+++ docs/management/commands/update_index.py
@@ -1,5 +1,4 @@
 import datetime
-from optparse import make_option as Option
 
 from django.core.management.base import BaseCommand
 
@@ -7,10 +6,6 @@ from ...search import DocumentDocType
 
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        Option("--using", default=None,
-               help='The name of the connection to use'),
-    )
 
     def log(self, msg, level=2):
         """
@@ -22,10 +17,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.verbosity = options['verbosity']
         new_index_name = 'docs-%s' % datetime.datetime.now().isoformat().lower()
-        index_results = DocumentDocType.index_all(
-            index_name=new_index_name,
-            using=options['using'],
-        )
+        index_results = DocumentDocType.index_all(index_name=new_index_name)
         for ok, item in index_results:
             id_ = item.get('index', {}).get('_id', item)
             if ok:
@@ -33,4 +25,4 @@ class Command(BaseCommand):
             else:
                 self.log('Failed indexing item %s' % id_)
         # Alias the main 'docs' index to the new one.
-        DocumentDocType.alias_to_main_index(new_index_name, using=options['using'])
+        DocumentDocType.alias_to_main_index(new_index_name)
