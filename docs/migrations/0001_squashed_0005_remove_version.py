d107d78f2de886a379edb5d87a969d793f4d53b8
Tim Graham <timograham@gmail.com>
2017-09-22 20:14:37 -0400

    Updated to Django 2.0.6

--- docs/migrations/0001_squashed_0005_remove_version.py
+++ docs/migrations/0001_squashed_0005_remove_version.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('lang', models.CharField(choices=settings.LANGUAGES, default='en', max_length=2)),
                 ('is_default', models.BooleanField(default=False)),
-                ('release', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='releases.Release')),
+                ('release', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='releases.Release', limit_choices_to={'status': 'f'})),
             ],
         ),
         migrations.AlterUniqueTogether(
