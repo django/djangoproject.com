django/djangoproject.com

commit 1be9a483fe876d36de35c5ece1000f16eadb0f13

Updated docs search to use PostgreSQL full-text search.

docs/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sitemaps.views import x_robots_tag
 from django.contrib.sites.models import Site
-from django.core.paginator import InvalidPage
+from django.core.paginator import InvalidPage, Paginator
 from django.http import Http404, JsonResponse
 from django.shortcuts import redirect, render
 from django.template.response import TemplateResponse
@@ -13,11 +13,9 @@ from django.utils.translation import activate, ugettext_lazy as _
 from django.views import static
 from django.views.decorators.cache import cache_page
 from django_hosts.resolvers import reverse
-from elasticsearch_dsl import query
 
 from .forms import DocSearchForm
 from .models import Document, DocumentRelease
-from .search import DocumentDocType, SearchPaginator
 from .utils import get_doc_path_or_404, get_doc_root_or_404
 
 SIMPLE_SEARCH_OPERATORS = ['+', '|', '-', '"', '*', '(', ')', '~']
@@ -162,36 +160,14 @@ def search_results(request, lang, version, per_page=10, orphans=3):
 
         if q:
             # catch queries that are coming from browser search bars
-            exact = (DocumentDocType.index_queryset()
-                                    .filter(release=release, title=q)
-                                    .first())
+            exact = Document.objects.filter(release=release, title=q).first()
             if exact is not None:
                 return redirect(exact)
 
-            # let's just use simple queries since they allow some
-            # neat syntaxes for exclusion etc. For more info see
-            # http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/query-dsl-simple-query-string-query.html
-            should = [
-                query.Common(_all={'query': q, 'cutoff_frequency': 0.001}),
-                query.SimpleQueryString(fields=['title', '_all'],
-                                        query=q,
-                                        default_operator='and'),
-            ]
-
-            # then apply the queries and filter out anything not matching
-            # the wanted version and language, also highlight the content
-            # and order the highlighted snippets by score so that the most
-            # fitting result is used
-            results = (DocumentDocType.search()
-                                      .query(query.Bool(should=should))
-                                      .filter('term', release__lang=release.lang)
-                                      .filter('term', release__version=release.version)
-                                      .highlight_options(order='score')
-                                      .highlight('content_raw')
-                                      .extra(min_score=.01))
+            results = Document.objects.search(q, release)
 
             page_number = request.GET.get('page') or 1
-            paginator = SearchPaginator(results, per_page=per_page, orphans=orphans)
+            paginator = Paginator(results, per_page=per_page, orphans=orphans)
 
             try:
                 page_number = int(page_number)
@@ -243,21 +219,17 @@ def search_suggestions(request, lang, version, per_page=20):
     if form.is_valid():
         q = form.cleaned_data.get('q')
         if q:
-            search = DocumentDocType.search()
-            search = (search.query(query.SimpleQueryString(fields=['title^10',
-                                                                   'content'],
-                                                           query=q,
-                                                           analyzer='stop',
-                                                           default_operator='and'))
-                            .filter('term', release__lang=release.lang)
-                            .filter('term', release__version=release.version)
-                            .source(includes=['title']))
-
+            results = Document.objects.filter(
+                release__lang=release.lang,
+            ) .filter(
+                release__version=release.version,
+            ).filter(
+                title__contains=q,
+            )
             suggestions.append(q)
             titles = []
             links = []
             content_type = ContentType.objects.get_for_model(Document)
-            results = search[0:per_page].execute()
             for result in results:
                 titles.append(result.title)
                 kwargs = {

.travis.yml
@@ -12,9 +12,9 @@ env:
 install:
   - pip install coveralls tox
 before_script:
-  - createuser -U postgres -d djangoproject
+  - createuser -U postgres -d djangoproject --superuser
   - createdb -U postgres -O djangoproject djangoproject
-  - createuser -U postgres -d code.djangoproject
+  - createuser -U postgres -d code.djangoproject --superuser
   - createdb -U postgres -O code.djangoproject code.djangoproject
   - psql -U postgres -d code.djangoproject < tracdb/trac.sql
 script:

djangoproject/scss/_style.scss
@@ -1026,14 +1026,6 @@ blockquote {
                 }
             }
 
-            &.elastic {
-                background: url(../img/logo-elastic.png) no-repeat left center;
-                .mdzr-svg & {
-                    background: url(../img/logo-elastic.svg) no-repeat left center;
-                    background-size: contain;
-                }
-            }
-
             &.threespot {
                 background: url(../img/logo-threespot.png) no-repeat left center;
                 .mdzr-svg & {

djangoproject/settings/common.py
@@ -80,6 +80,7 @@ INSTALLED_APPS = [
     'django.contrib.flatpages',
     'django.contrib.humanize',
     'django.contrib.messages',
+    'django.contrib.postgres',
     'django.contrib.redirects',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
@@ -265,6 +266,3 @@ THUMBNAIL_ALTERNATIVE_RESOLUTIONS = [2]
 # dashboard settings
 TRAC_RPC_URL = "https://code.djangoproject.com/rpc"
 TRAC_URL = "https://code.djangoproject.com/"
-
-# search settings
-ES_HOST = SECRETS.get('es_host', 'localhost:9200')

djangoproject/templates/docs/search_results.html
@@ -39,7 +39,7 @@
         <a href="{% url 'document-detail' lang=result.release.lang version=result.release.version url=result.path host 'docs' %}">{{ result.title }}</a>
       </h2>
       <span class="meta breadcrumbs">
-        {% for breadcrumb in result.breadcrumbs %}
+        {% for breadcrumb in result.metadata.breadcrumbs %}
         <a href="{% url 'document-detail' lang=result.release.lang version=result.release.version url=breadcrumb.path host 'docs' %}">{{ breadcrumb.title }}</a>{% if not forloop.last %} <span class="arrow">»</span>{% endif %}
         {% endfor %}
       </span>

djangoproject/templates/includes/footer.html
@@ -48,7 +48,6 @@
       <ul class="thanks">
         <li>
           <span>Hosting by</span> <a class="rackspace" href="https://www.rackspace.com">Rackspace</a>
-          <span>Search by</span> <a class="elastic" href="https://www.elastic.co">Elastic Search</a>
         </li>
         <li class="design"><span>Design by</span> <a class="threespot" href="https://www.threespot.com">Threespot</a> <span class="ampersand">&amp;</span> <a class="andrevv" href="http://andrevv.com/"></a></li>
       </ul>

docs/apps.py
@@ -1,22 +1,7 @@
-import certifi
 from django.apps import AppConfig
-from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-from elasticsearch_dsl.connections import connections
 
 
 class DocsConfig(AppConfig):
     name = 'docs'
     verbose_name = _('Documentation')
-
-    def ready(self):
-        super().ready()
-        # Configure Elasticsearch connections for connection pooling.
-        connections.configure(
-            default={
-                'hosts': settings.ES_HOST,
-                'verify_certs': True,
-                'ca_certs': certifi.where(),
-                'timeout': 60.0,
-            },
-        )

docs/management/commands/update_docs.py
@@ -24,7 +24,7 @@ class Command(BaseCommand):
             action='store_true',
             dest='update_index',
             default=False,
-            help='Also update the elasticsearch index.',
+            help='Also update the search vector field.',
         )
 
     def handle(self, **kwargs):

commit 4424902f55655fad583b6b178ad67cae33c83bac

Fixed crash in search_suggestions view.
Regression in 1be9a483fe876d36de35c5ece1000f16eadb0f13.

docs/views.py
@@ -221,8 +221,8 @@ def search_suggestions(request, lang, version, per_page=20):
         if q:
             results = Document.objects.filter(
                 release__lang=release.lang,
-            ) .filter(
-                release__version=release.version,
+            ).filter(
+                release__release__version=release.version,
             ).filter(
                 title__contains=q,
             )
@@ -234,7 +234,7 @@ def search_suggestions(request, lang, version, per_page=20):
                 titles.append(result.title)
                 kwargs = {
                     'content_type_id': content_type.pk,
-                    'object_id': result.meta.id,
+                    'object_id': result.id,
                 }
                 links.append(reverse('contenttypes-shortcut', kwargs=kwargs))
             suggestions.append(titles)