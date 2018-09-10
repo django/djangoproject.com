3047b279c5c84491e119482ed10a0ea1cce5581d
Tim Graham <timograham@gmail.com>
2018-09-10 12:05:52 -0400

    Updated CSS and "copy to clipboard" for replacing snippet with code-block.
    refs #20910.

--- djangoproject/static/js/mod/clippify.js
+++ djangoproject/static/js/mod/clippify.js
@@ -1,4 +1,14 @@
 define(['jquery', 'clipboard'], function($, Clipboard) {
+    $('.code-block-caption').each(function() {
+        var header = $(this);
+        var wrapper = header.parent();
+        var code = $('.highlight', wrapper);
+        var btn = $('<span class="btn-clipboard" title="Copy this code">');
+        btn.append('<i class="icon icon-clipboard">');
+        btn.data('clipboard-text', $.trim(code.text()));
+        header.append(btn);
+    });
+    // For Django 2.0 docs and older.
     $('.snippet').each(function() {
         var code = $('.highlight', this);
         var btn = $('<span class="btn-clipboard" title="Copy this code">');
