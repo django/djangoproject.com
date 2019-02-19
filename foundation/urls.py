f65ed90c9d1bda98b5a097cd7d967268b8552f86
saurabh <srbhsingh39@gmail.com>
2019-02-19 07:23:17 +0530

    Commented BoardMinutes generator view. Now, Add BoardMinutes from admin panel.

--- foundation/urls.py
+++ foundation/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import BoardMinutesCreateView, BoardMinutesRecordListView, BoardMinutesDetailView
+from .views import BoardMinutesRecordListView, BoardMinutesDetailView
 
-urlpatterns = [ 
-    path('records/minutes/add/', BoardMinutesCreateView.as_view(), name='board-minutes-add'),
+urlpatterns = [
+    # path('records/minutes/add/', BoardMinutesCreateView.as_view(), name='board-minutes-add'),
     path('records/minutes/', BoardMinutesRecordListView.as_view(), name='board-minutes-list'),
     path('records/minutes/<slug>/', BoardMinutesDetailView.as_view(), name='board-minutes-detail')
 ]
