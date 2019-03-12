4f18ec8633a995224bd0e9164a5615d5a0d5c072
Tobias McNulty <tobias@caktusgroup.com>
2019-03-11 20:33:23 -0400

    Reorganized locale directories and added Transifex config for managing the translation process (#866)

---
+++ update-translations.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Update translations from Transifex for the supported languages, and remove
+# the source location references (to help keep the diff noise down). Adapted from:
+# https://github.com/django/django-docs-translations/blob/stable/2.1.x/manage_translations.py
+
+set -ex
+
+LANGUAGES="fr"
+
+LOCALE_DIRS="dashboard/locale/ docs/locale/ locale/"
+
+for LANG in $LANGUAGES; do
+    tx pull -f -l $LANG --minimum-perc=70
+    for DIR in $LOCALE_DIRS; do
+        PO_FILE="$DIR$LANG/LC_MESSAGES/django.po"
+        if [ -f "$PO_FILE" ]; then
+            msgcat --no-location -o $PO_FILE $PO_FILE
+        fi
+    done
+done
