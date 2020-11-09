6c2161caa2c706ea17165b72154604dc5deae60c
James Bennett <james@b-list.org>
2020-11-09 00:04:59 -0800

    Add support for listing recipients of the Core Developer title.

--- foundation/admin.py
+++ foundation/admin.py
@@ -15,14 +15,15 @@ class TermAdmin(admin.ModelAdmin):
 
 @admin.register(models.BoardMember)
 class BoardMemberAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'office', 'term')
-    list_filter = ('office', 'term')
+    list_display = ("full_name", "office", "term")
+    list_filter = ("office", "term")
     list_select_related = True
-    raw_id_fields = ('account',)
+    raw_id_fields = ("account",)
 
     def full_name(self, obj):
         return obj.account.get_full_name()
-    full_name.admin_order_field = 'account__last_name'
+
+    full_name.admin_order_field = "account__last_name"
 
 
 @admin.register(models.NonBoardAttendee)
@@ -53,21 +54,37 @@ class ActionItemInline(admin.StackedInline):
 @admin.register(models.Meeting)
 class MeetingAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Metadata', {
-            'fields': ('title', 'slug', 'date', 'leader', 'board_attendees', 'non_board_attendees'),
-        }),
-        ('Treasurer report', {
-            'fields': ('treasurer_balance', 'treasurer_report'),
-        }),
+        (
+            "Metadata",
+            {
+                "fields": (
+                    "title",
+                    "slug",
+                    "date",
+                    "leader",
+                    "board_attendees",
+                    "non_board_attendees",
+                ),
+            },
+        ),
+        (
+            "Treasurer report",
+            {
+                "fields": ("treasurer_balance", "treasurer_report"),
+            },
+        ),
     )
-    filter_horizontal = ('board_attendees', 'non_board_attendees')
+    filter_horizontal = ("board_attendees", "non_board_attendees")
     inlines = [
         GrantInline,
         IndividualMemberInline,
         CorporateMemberInline,
         BusinessInline,
-        ActionItemInline
+        ActionItemInline,
     ]
-    list_display = ('title', 'date')
-    list_filter = ('date',)
-    prepopulated_fields = {'slug': ('title',)}
+    list_display = ("title", "date")
+    list_filter = ("date",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register([models.CoreAwardCohort, models.CoreAward])
