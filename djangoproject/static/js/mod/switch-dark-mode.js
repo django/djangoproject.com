33413817b5bc365fd2f2c978cff00ae860e9c1a4
Sarah Abderemane <sarahabderemane@gmail.com>
2022-08-02 00:55:14 +0200

    Review icons and style with django admin dark mode

--- djangoproject/static/js/mod/switch-dark-mode.js
+++ djangoproject/static/js/mod/switch-dark-mode.js
@@ -1,46 +1,59 @@
 define([
-	'jquery' //requires jquery
+    'jquery' //requires jquery
 ], function ($) {
 
-	$(document).on('ready', function () {
-		const useDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)");
-		const theme = window.localStorage.getItem('dark-mode');
+    function setTheme(mode) {
+        if (mode !== "light" && mode !== "dark" && mode !== "auto") {
+            console.error(`Got invalid theme mode: ${mode}. Resetting to auto.`);
+            mode = "auto";
+        }
+        document.documentElement.dataset.theme = mode;
+        localStorage.setItem("theme", mode);
+    }
 
-		// Toggles the "dark-mode" class based on if the media query matches or if theme exist
-		function toggleDarkMode(state, theme) {
-			if (theme != null) {
-				if (theme == 'true') {
-					$('html').addClass("dark-mode");
-				} else {
-					$('html').removeClass("dark-mode");
-				}
-				$('#toggle').attr('aria-checked', theme);
+    function cycleTheme() {
+        const currentTheme = localStorage.getItem("theme") || "auto";
+        const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-			} else {
-				$('html').toggleClass("dark-mode", state);
-				$('#toggle').attr('aria-checked', state);
-			}
-		}
+        if (prefersDark) {
+            // Auto (dark) -> Light -> Dark
+            if (currentTheme === "auto") {
+                setTheme("light");
+            } else if (currentTheme === "light") {
+                setTheme("dark");
+            } else {
+                setTheme("auto");
+            }
+        } else {
+            // Auto (light) -> Dark -> Light
+            if (currentTheme === "auto") {
+                setTheme("dark");
+            } else if (currentTheme === "dark") {
+                setTheme("light");
+            } else {
+                setTheme("auto");
+            }
+        }
+    }
 
-		// Initial setting
-		if (theme) {
-			toggleDarkMode(theme, theme);
-		} else {
-			toggleDarkMode(useDarkMode.matches, null);
-		}
+    function initTheme() {
+        // set theme defined in localStorage if there is one, or fallback to auto mode
+        const currentTheme = localStorage.getItem("theme");
+        currentTheme ? setTheme(currentTheme) : setTheme("auto");
+    }
 
-		// Listen for changes in the system preferences
-		$(useDarkMode).on('change', function (evt) { toggleDarkMode(evt.target.matches, theme) });
+    function setupTheme() {
+        // Attach event handlers for toggling themes
+        const buttons = document.getElementsByClassName("theme-toggle");
+        Array.from(buttons).forEach((btn) => {
+            btn.addEventListener("click", cycleTheme);
+        });
+        initTheme();
+    }
 
-		// Toggles the "dark-mode" class on click
-		$("#toggle").on("click", () => {
-			$('html').toggleClass("dark-mode");
-			const state = $('#toggle').attr('aria-checked');
-			const newState = (state == 'true') ? false : true;
-			$('#toggle').attr('aria-checked', newState);
-			window.localStorage.setItem('dark-mode', newState);
-		});
-	});
+
+
+    $(document).on('ready', function () {
+        setupTheme();
+    });
 })
-
-
