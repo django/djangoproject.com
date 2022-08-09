78a6cb96de28c9c9581656492a057d88a3755a44
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-08-09 09:15:18 +0200

    Increased FeedItem.link/Feed.public_url max_length to 1023 characters.

---
+++ aggregator/migrations/0003_increase_url_max_length.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aggregator', '0002_add_feed_approver_auth_group'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='feed',
+            name='public_url',
+            field=models.URLField(max_length=1023),
+        ),
+        migrations.AlterField(
+            model_name='feeditem',
+            name='link',
+            field=models.URLField(max_length=1023),
+        ),
+    ]
