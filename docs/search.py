edee0dd55d6492acb52ce85aab040084e4b109b8
Paolo Melchiorre <paolo@20tab.com>
2022-09-14 21:23:33 +0200

    Fixed #1200 -- Add more languages search support (#1201)

--- docs/search.py
+++ docs/search.py
@@ -2,25 +2,32 @@ from django.contrib.postgres.search import SearchVector
 from django.db.models import F
 from django.db.models.fields.json import KeyTextTransform
 
-# Imported from https://github.com/postgres/postgres/blob/REL_10_STABLE/src/bin/initdb/initdb.c#L664
+# Imported from https://github.com/postgres/postgres/blob/REL_12_STABLE/src/bin/initdb/initdb.c#L701
 TSEARCH_CONFIG_LANGUAGES = {
+    'ar': 'arabic',
     'da': 'danish',
     'de': 'german',
     'en': 'english',
     'es': 'spanish',
     'fi': 'finnish',
     'fr': 'french',
+    'ga': 'irish',
     'hu': 'hungarian',
+    'id': 'indonesian',
     'it': 'italian',
+    'lt': 'lithuanian',
+    'ne': 'nepali',
     'nl': 'dutch',
     'no': 'norwegian',
     'pt': 'portuguese',
     'ro': 'romanian',
     'ru': 'russian',
     'sv': 'swedish',
+    'ta': 'tamil',
     'tr': 'turkish',
 }
-# Imported from https://github.com/postgres/postgres/blob/REL_10_STABLE/src/bin/initdb/initdb.c#L2599
+
+# Imported from https://github.com/postgres/postgres/blob/REL_12_STABLE/src/bin/initdb/initdb.c#L2668
 DEFAULT_TEXT_SEARCH_CONFIG = 'simple'
 
 DOCUMENT_SEARCH_VECTOR = (
