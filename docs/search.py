aa0f61a1e5b95aaf8d61450170917d5a7eface11
Paolo Melchiorre <paolo@melchiorre.org>
2022-09-22 09:49:18 +0100

    Fixed #1211 -- Align the languages supported with PostgreSQL 14

--- docs/search.py
+++ docs/search.py
@@ -2,17 +2,22 @@ from django.contrib.postgres.search import SearchVector
 from django.db.models import F
 from django.db.models.fields.json import KeyTextTransform
 
-# Imported from https://github.com/postgres/postgres/blob/REL_12_STABLE/src/bin/initdb/initdb.c#L701
+# Imported from https://github.com/postgres/postgres/blob/REL_14_STABLE/src/bin/initdb/initdb.c#L659
 TSEARCH_CONFIG_LANGUAGES = {
     'ar': 'arabic',
+    'ca': 'catalan',
     'da': 'danish',
     'de': 'german',
+    'el': 'greek',
     'en': 'english',
     'es': 'spanish',
+    'eu': 'basque',
     'fi': 'finnish',
     'fr': 'french',
     'ga': 'irish',
+    'hi': 'hindi',
     'hu': 'hungarian',
+    'hy': 'armenian',
     'id': 'indonesian',
     'it': 'italian',
     'lt': 'lithuanian',
@@ -22,12 +27,14 @@ TSEARCH_CONFIG_LANGUAGES = {
     'pt': 'portuguese',
     'ro': 'romanian',
     'ru': 'russian',
+    'sr': 'serbian',
     'sv': 'swedish',
     'ta': 'tamil',
     'tr': 'turkish',
+    'yi': 'yiddish',
 }
 
-# Imported from https://github.com/postgres/postgres/blob/REL_12_STABLE/src/bin/initdb/initdb.c#L2668
+# Imported from https://github.com/postgres/postgres/blob/REL_14_STABLE/src/bin/initdb/initdb.c#L2557
 DEFAULT_TEXT_SEARCH_CONFIG = 'simple'
 
 DOCUMENT_SEARCH_VECTOR = (
