07e6453f4e981bb406bc5bcb9984c73ec0dfdba7
pavel <rodionov134@gmail.com>
2023-11-25 03:28:00 +0300

    Fix #1423 Flash of unstyled mobile menu content (#1424)
    * fix flash of unstyled mobile menu content django#1423
    
    * Fix accessibility menu button
    
    ---------
    
    Co-authored-by: Sarah Abderemane <sarahabderemane@gmail.com>

--- djangoproject/static/js/mod/mobile-menu.js
+++ djangoproject/static/js/mod/mobile-menu.js
@@ -4,19 +4,16 @@ define([
 
     var MobileMenuExport = function(menu) {
         this.menu = $(menu); //menu container
-        this.menuBtn = $('.mobile-toggle'); // toggle dark mode icon
+        this.toggleMenuBtn = $('.menu-button')
         this.init();
     };
 
     MobileMenuExport.prototype = {
         init: function(){
             var self = this;
-            self.menu.addClass('nav-menu-on');
-            self.button = $('<button class="menu-button"><i class="icon icon-reorder"></i><span>Menu</span></button>');
-            self.button.insertBefore(self.menuBtn);
-            self.button.on( 'click', function(){
+            self.toggleMenuBtn.on( 'click', function(){
                 self.menu.toggleClass('active');
-                self.button.toggleClass('active');
+                self.toggleMenuBtn.toggleClass('active');
             });
         }
     };
