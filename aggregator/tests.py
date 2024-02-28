fd9c5d1ec4ae2b935e6bb3470fd479e02eab23e6
Baptiste Mispelon <bmispelon@gmail.com>
2024-02-28 10:36:12 +0100

    Fixed doc_test_fixtures.json.
    The broken fixture was hidden by the cache not being reset between
    tests. This meant that some tests would fail when run in isolation,
    but pass when run as part of the whole suite (because previous tests
    would warm up the cache).

--- aggregator/tests.py
+++ aggregator/tests.py
@@ -91,7 +91,7 @@ class AggregatorTests(TestCase):
     def test_community_index_number_of_queries(self):
         """Intended to prevent an n+1 issue on the community index view"""
         url = reverse("community-index")
-        with self.assertNumQueries(4):
+        with self.assertNumQueries(6):
             self.client.get(url)
 
     def test_feed_list_only_approved_and_active(self):
@@ -106,7 +106,7 @@ class AggregatorTests(TestCase):
         url = reverse(
             "community-feed-list", kwargs={"feed_type_slug": self.feed_type.slug}
         )
-        with self.assertNumQueries(5):
+        with self.assertNumQueries(7):
             self.client.get(url)
 
     def test_management_command_sends_no_email_with_no_pending_feeds(self):
