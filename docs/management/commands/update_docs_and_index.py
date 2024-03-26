69a0c1860fd13bc44a7726f71f797d292f1b2faa
Cory Zue <cory@coryzue.com>
2024-03-26 09:38:22 +0200

    make it possible to force build from update_docs_and_index

--- docs/management/commands/update_docs_and_index.py
+++ docs/management/commands/update_docs_and_index.py
@@ -7,5 +7,16 @@ class Command(BaseCommand):
     Update the docs then reindex them in the search vector field.
     """
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--force",
+            action="store_true",
+            default=False,
+            help=(
+                "Force docs update even if docs in git didn't change or the "
+                "version is no longer supported."
+            ),
+        )
+
     def handle(self, **options):
         call_command("update_docs", update_index=True, **options)
