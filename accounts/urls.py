c1e817cea01f0f1755afbf5fb0eac55128d96a92
Baptiste Mispelon <bmispelon@gmail.com>
2024-06-09 11:27:39 +0200

    Removed unused URL
    Trac removed the core/CLA badges in 2016 (7c6566898409b8300aa60600cc5b579fd84425b1).

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
         account_views.edit_profile,
         name="edit_profile",
     ),
-    path("_trac/userinfo/", account_views.json_user_info),
     path("", include("django.contrib.auth.urls")),
     path("", include("registration.backends.default.urls")),
 ]
