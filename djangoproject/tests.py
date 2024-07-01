f90b46dc1637cdff06c08466f5cc0c2299f2c705
Lakshya272 <87129880+Lakshya272@users.noreply.github.com>
2024-07-01 20:49:46 +0530

    Fixed #1562 -- Added test for pending migrations

--- djangoproject/tests.py
+++ djangoproject/tests.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
+from io import StringIO
 
+from django.core.management import call_command
 from django.test import TestCase
 from django_hosts.resolvers import reverse
 
@@ -114,3 +116,17 @@ class ExcludeHostsLocaleMiddlewareTests(TestCase):
         self.assertEqual(resp.status_code, HTTPStatus.OK)
         self.assertIn("Content-Language", resp)
         self.assertIn("Vary", resp)
+
+
+class PendingMigrationsTests(TestCase):
+    def test_no_pending_migrations(self):
+        out = StringIO()
+        try:
+            call_command(
+                "makemigrations",
+                "--check",
+                stdout=out,
+                stderr=StringIO(),
+            )
+        except SystemExit:  # pragma: no cover
+            raise AssertionError("Pending migrations:\n" + out.getvalue()) from None
