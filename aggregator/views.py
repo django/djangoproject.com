887ed4b94ca01434eb275bf4289cd6cf19e62fa9
Mark Walker <mark@marksweb.co.uk>
2024-10-13 19:24:07 +0100

    chore: Add translation tags to aggregator app

--- aggregator/views.py
+++ aggregator/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic.list import ListView
+from django.utils.translation import gettext_lazy as _
 
 from .forms import FeedModelForm
 from .models import APPROVED_FEED, Feed, FeedItem, FeedType, LocalDjangoCommunity
@@ -70,8 +71,8 @@ def add_feed(request, feed_type_slug):
         messages.add_message(
             request,
             messages.INFO,
-            "Your feed has entered moderation. "
-            "Please allow up to 1 week for processing.",
+            _("Your feed has entered moderation. "
+            "Please allow up to 1 week for processing."),
         )
         return redirect("community-index")
 
