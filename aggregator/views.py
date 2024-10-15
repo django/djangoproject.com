c0ad307abe778c6aaa983b7fa93131ebc08b99c5
Mark Walker <theshow@gmail.com>
2024-10-15 17:26:19 +0100

    Apply suggestions from code review
    Co-authored-by: Baptiste Mispelon <bmispelon@gmail.com>

--- aggregator/views.py
+++ aggregator/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from django.views.generic.list import ListView
 
 from .forms import FeedModelForm
