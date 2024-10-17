2308b78ab4a1661c915549252571b9bdf15da81f
Mark Walker <mark@marksweb.co.uk>
2024-10-17 21:07:06 +0100

    Revert "feat: Add custom formats"
    This reverts commit 036e16e73027f31d4c4f6ff8837d4ead7fe1b288.

--- djangoproject/formats/en/formats.py
+++
@@ -1,7 +0,0 @@
-# The *_FORMAT strings use the Django date format syntax,
-# see https://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
-
-# Month(long) day, year
-MONTH_DAY_YEAR_FORMAT = "F j, Y"
-
-YEAR_FORMAT = "Y"
