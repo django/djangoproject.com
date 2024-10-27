87c48e73cba75651ac54ad3d57f778ce49745875
Tobias McNulty <tobias@caktusgroup.com>
2024-10-27 10:17:04 -0400

    Removed production Docker entrypoint (#1699)

--- docker-entrypoint.prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python -m manage migrate
-python -m manage compilemessages
-make compile-scss # must come before collectstatic
-python -m manage collectstatic --no-input --clear
-
-exec "$@"
