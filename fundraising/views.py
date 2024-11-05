django/djangoproject.com

commit a41119e30356a4147b4fb857e83b6c51a931fac4

[fundraising] Marked all user-facing strings for translation
Refs #1648

fundraising/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
 from django.urls import reverse
+from django.utils.translation import gettext as _
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
@@ -131,7 +132,7 @@ def manage_donations(request, hero):
         if hero_form.is_valid() and modify_donations_formset.is_valid():
             hero_form.save()
             modify_donations_formset.save()
-            messages.success(request, "Your information has been updated.")
+            messages.success(request, _("Your information has been updated."))
     else:
         hero_form = DjangoHeroForm(instance=hero)
         modify_donations_formset = ModifyDonationsFormset(queryset=recurring_donations)
@@ -178,7 +179,7 @@ def cancel_donation(request, hero):
     donation.stripe_subscription_id = ""
     donation.save()
 
-    messages.success(request, "Your donation has been canceled.")
+    messages.success(request, _("Your donation has been canceled."))
     return redirect("fundraising:manage-donations", hero=hero.pk)
 
 
@@ -240,7 +241,7 @@ class WebhookHandler:
             "fundraising/email/subscription_cancelled.txt", {"donation": donation}
         )
         send_mail(
-            "Payment cancelled",
+            _("Payment cancelled"),
             mail_text,
             settings.DEFAULT_FROM_EMAIL,
             [donation.donor.email],
@@ -258,7 +259,7 @@ class WebhookHandler:
             "fundraising/email/payment_failed.txt", {"donation": donation}
         )
         send_mail(
-            "Payment failed",
+            _("Payment failed"),
             mail_text,
             settings.DEFAULT_FROM_EMAIL,
             [donation.donor.email],
@@ -328,7 +329,7 @@ class WebhookHandler:
             "fundraising/email/thank-you.html", {"donation": donation}
         )
         send_mail(
-            "Thank you for your donation to the Django Software Foundation",
+            _("Thank you for your donation to the Django Software Foundation"),
             message,
             settings.FUNDRAISING_DEFAULT_FROM_EMAIL,
             [donation.receipt_email],

djangoproject/templates/fundraising/email/thank-you.html
@@ -1,11 +1,15 @@
-Hello!
+{% load i18n %}{% spaceless %}{% url 'fundraising:manage-donations' donation.donor.pk as manage_url %}{% blocktranslate with manage_url=manage_url %}
+    Hello!
 
-Thank you for making a donation to the Django Software Foundation.
+    Thank you for making a donation to the Django Software Foundation.
 
-Your support is invaluable to continue the rapid development of Django and helps the Django Fellowship program in particular.
+    Your support is invaluable to continue the rapid development of Django and helps the Django Fellowship program in particular.
 
-If you want to change information about your donation, you can do so by accessing the link below:
-{% url 'fundraising:manage-donations' donation.donor.pk %}
+    If you want to change information about your donation, you can do so by accessing the link below:
 
-Best regards,
-Django Software Foundation
+    {{ manage_url }}
+
+    Best regards,
+    Django Software Foundation
+{% endblocktranslate %}
+{% endspaceless %}

djangoproject/templates/fundraising/includes/display_django_heroes.html
@@ -1,7 +1,8 @@
-{% load humanize %}
+{% load humanize i18n %}
 <div class="heroes">
   {% if corporate_members.diamond %}
-    <h3>Diamond Corporate Members (${{ corporate_membership_amounts.diamond|intcomma }}+)</h3>
+    <h3>{% blocktranslate trimmed with amount=corporate_membership_amounts.diamond|intcomma %}
+      Diamond Corporate Members (${{ amount }}+){% endblocktranslate %}</h3>
     <div>
       {% for obj in corporate_members.diamond %}
         {% include "fundraising/includes/_hero_with_logo.html" %}
@@ -10,7 +11,8 @@
   {% endif %}
 
   {% if corporate_members.platinum %}
-    <h3>Platinum Corporate Members (${{ corporate_membership_amounts.platinum|intcomma }}+)</h3>
+    <h3>{% blocktranslate trimmed with amount=corporate_membership_amounts.platinum|intcomma %}
+      Platinum Corporate Members (${{ amount }}+){% endblocktranslate %}</h3>
     <div>
       {% for obj in corporate_members.platinum %}
         {% include "fundraising/includes/_hero_with_logo.html" %}
@@ -19,7 +21,8 @@
   {% endif %}
 
   {% if corporate_members.gold %}
-    <h3>Gold Corporate Members (${{ corporate_membership_amounts.gold|intcomma }}+)</h3>
+    <h3>{% blocktranslate trimmed with amount=corporate_membership_amounts.gold|intcomma %}
+      Gold Corporate Members (${{ amount }}+){% endblocktranslate %}</h3>
     <div>
       {% for obj in corporate_members.gold %}
         {% include "fundraising/includes/_hero_with_logo.html" %}
@@ -28,7 +31,8 @@
   {% endif %}
 
   {% if corporate_members.silver %}
-    <h3>Silver Corporate Members (${{ corporate_membership_amounts.silver|intcomma }}+)</h3>
+    <h3>{% blocktranslate trimmed with amount=corporate_membership_amounts.silver|intcomma %}
+      Silver Corporate Members (${{ amount }}+){% endblocktranslate %}</h3>
     <div>
       {% for obj in corporate_members.silver %}
         {% include "fundraising/includes/_hero_with_logo.html" %}
@@ -37,7 +41,8 @@
   {% endif %}
 
   {% if corporate_members.bronze %}
-    <h3>Bronze Corporate Members (${{ corporate_membership_amounts.bronze|intcomma }}+)</h3>
+    <h3>{% blocktranslate trimmed with amount=corporate_membership_amounts.bronze|intcomma %}
+      Bronze Corporate Members (${{ amount }}+){% endblocktranslate %}</h3>
     <div>
       {% for obj in corporate_members.bronze %}
         {% include "fundraising/includes/_hero_with_logo.html" %}
@@ -45,8 +50,8 @@
     </div>
   {% endif %}
 
-  <h3><a id="in-kind-donors" href="#in-kind-donors">In-kind donors</a></h3>
-  <p>These donors help with significant non-cash contributions.</p>
+  <h3><a id="in-kind-donors" href="#in-kind-donors">{% translate "In-kind donors" %}</a></h3>
+  <p>{% translate "These donors help with significant non-cash contributions." %}</p>
   <div>
     {% with show_description=True %}
       {% for obj in inkind_donors %}
@@ -55,17 +60,26 @@
     {% endwith %}
   </div>
 
-  <h3>Leaders (${{ display_logo_amount|intcomma }}+)</h3>
-  <p>Leadership-level donors contribute ${{ display_logo_amount|intcomma }} or
-    more in a calendar year.</p>
+  <h3>{% blocktranslate trimmed with amount=display_logo_amount|intcomma %}
+    Leaders (${{ amount }}+){% endblocktranslate %}</h3>
+  <p>
+    {% blocktranslate trimmed with amount=display_logo_amount|intcomma %}
+      Leadership-level donors contribute ${{ amount }} or
+      more in a calendar year.
+    {% endblocktranslate %}
+  </p>
   <div>
     {% for obj in leaders %}
       {% include "fundraising/includes/_hero_with_logo.html" %}
     {% endfor %}
   </div>
 
-  <h3>Heroes</h3>
-  <p>Our donor roll for all donations made in the last {{ display_donor_days }} days.</p>
+  <h3>{% translate "Heroes" %}</h3>
+  <p>
+    {% blocktranslate trimmed %}
+      Our donor roll for all donations made in the last {{ display_donor_days }} days.
+    {% endblocktranslate %}
+  </p>
   <div>
     {% for donor in heroes %}
       <div class="no-logo-hero">

djangoproject/templates/fundraising/includes/donation_form_with_heart.html
@@ -1,4 +1,4 @@
-{% load static fundraising_extras humanize %}
+{% load fundraising_extras humanize i18n static %}
 
 {% with goal_percent=donated_amount|as_percentage:goal_amount %}
   <div class="fundraising-index" id="donate">
@@ -21,13 +21,19 @@
 
     <div class="description">
       <ul>
-        <li><strong>{{ goal_percent }}% funded</strong></li>
-        <li><strong>${{ donated_amount|intcomma }} donated</strong> of US&nbsp;${{ goal_amount|intcomma }} goal for
-          {{ goal_start_date|date:"Y" }}</li>
+        <li><strong>{% blocktranslate trimmed %}{{ goal_percent }}% funded{% endblocktranslate %}</strong></li>
         <li>
-          Companies able to make a <strong>larger donation</strong>
-          ($2,000+/year) are invited to apply to be Corporate Members
-          <a href="{% url 'members:corporate-members-join' %}">here</a>.
+          {% blocktranslate trimmed with amount=donated_amount|intcomma goal=goal_amount|intcomma start_date=goal_start_date|date:"Y" %}
+            <strong>${{ amount }} donated</strong> of US&nbsp;${{ goal }} goal for {{ start_date }}
+          {% endblocktranslate %}
+        </li>
+        <li>
+          {% url 'members:corporate-members-join' as corp_join_url %}
+          {% blocktranslate trimmed %}
+            Companies able to make a <strong>larger donation</strong>
+            ($2,000+/year) are invited to apply to be Corporate Members
+            <a href="{{ corp_join_url }}">here</a>.
+          {% endblocktranslate %}
         </li>
       </ul>
 
@@ -41,21 +47,23 @@
           data-is-logged-in="{{ user.is_authenticated }}"
           data-login-url="{% url 'login' %}">
           {% csrf_token %}
-          <h3>Help us make it happen:</h3>
+          <h3>{% translate "Help us make it happen:" %}</h3>
           {{ form.interval }}
           {{ form.amount }}
           <div class="custom-donation">
-            <span class="prefix">US $ (integer only)</span>
+            <span class="prefix">US $ ({% translate "integer only" context "Donation amount in US dollars" %})</span>
           </div>
           {{ form.captcha }}
-          <input id="donate-button" type="submit" value="Donate monthly" class="cta" />
+          <input id="donate-button" type="submit" value="{% translate "Donate monthly" %}" class="cta" />
         </form>
       </div>
     </div>
     <div class="cls"></div>
     <p class="footnote">
-      Your logo will be visible below if you contribute at least
-      US&nbsp;${{ display_logo_amount|intcomma }}.<br>
+      {% blocktranslate trimmed with amount=display_logo_amount|intcomma %}
+        Your logo will be visible below if you contribute at least
+        US&nbsp;${{ amount }}.<br>
+      {% endblocktranslate %}
     </p>
   </div>
 {% endwith %}

djangoproject/templates/fundraising/includes/donation_snippet.html
@@ -1,8 +1,8 @@
-{% load humanize static %}
+{% load humanize i18n static %}
 
 {% if donation %}
   <div class="fundraising-sidebar">
-    <h2>Support Django!</h2>
+    <h2>{% translate "Support Django!" %}</h2>
 
     <div class="small-heart">
       <img src="{% static "img/fundraising-heart.svg" %}" alt="Support Django!" />
@@ -11,8 +11,10 @@
     <div class="small-cta">
       <ul class="list-links-small">
         <li><a href="{% url "fundraising:index" %}">
-          {{ donation.name }} donated to the Django Software Foundation to
-          support Django development. Donate today!
+          {% blocktranslate trimmed with name=donation.name %}
+            {{ name }} donated to the Django Software Foundation to
+            support Django development. Donate today!
+          {% endblocktranslate %}
         </a></li>
       </ul>
     </div>

djangoproject/templates/fundraising/manage-donations.html
@@ -1,5 +1,5 @@
 {% extends 'fundraising/index.html' %}
-{% load static %}
+{% load i18n static %}
 
 {% block messages %}
 
@@ -14,27 +14,31 @@
 
 {% block content %}
 
-  <h1>Manage your donations to the Django Software Foundation</h1>
-  <p>Your support is <strong>invaluable</strong> to continue the rapid development
-    of Django and helps the <strong>Django Fellowship</strong> program in particular.
-    Thank you!</p>
+  <h1>{% translate "Manage your donations to the Django Software Foundation" %}</h1>
+  <p>
+    {% blocktranslate trimmed %}
+      Your support is <strong>invaluable</strong> to continue the rapid development
+      of Django and helps the <strong>Django Fellowship</strong> program in particular.
+      Thank you!
+    {% endblocktranslate %}
+  </p>
 
   <form enctype="multipart/form-data" action="" method="post" class="form-input django-hero-form"
         data-stripe-key="{{ stripe_publishable_key }}" data-stripe-icon="{% static 'img/dj-stripe-icon.jpg' %}"
         data-update-card-url="{% url 'fundraising:update-card' %}">
-    <h2>Manage your participation in the fundraising campaigns</h2>
-    <p>Information entered below will be visible on all of your donations to the Django Project.</p>
+    <h2>{% translate "Manage your participation in the fundraising campaigns" %}</h2>
+    <p>{% translate "Information entered below will be visible on all of your donations to the Django Project." %}</p>
     {% csrf_token %}
     {% include 'fundraising/includes/_form.html' with form=hero_form %}
     <p class="submit">
-      <input type="submit" class="cta" value="Save &rarr;">
+      <input type="submit" class="cta" value="{% translate "Save &rarr;" context "Save personal details about donation" %}">
     </p>
   {# Always include to avoid "Management form has been tampered with" if no recurring donations exist #}
     {{ modify_donations_formset.management_form }}
     {% if recurring_donations %}
       <hr />
-      <h2>Modify your recurring donations</h2>
-      <p>Update the time interval or amount of your recurring donation here:</p>
+      <h2>{% translate "Modify your recurring donations" %}</h2>
+      <p>{% translate "Update the time interval or amount of your recurring donation here:" %}</p>
       {% for form in modify_donations_formset %}
         {% include 'fundraising/includes/_form.html' %}
         <div class="change-card-container">
@@ -44,20 +48,22 @@
         </div>
       {% endfor %}
       <p class="submit">
-        <input type="submit" class="cta" value="Save &rarr;">
+        <input type="submit" class="cta" value="{% translate "Save &rarr;" context "Save personal details about donation" %}">
       </p>
       </form>
       <hr />
-      <h2>Cancel your recurring donations</h2>
-      <p>You can cancel your recurring donation to the Django Software Foundation anytime.</p>
+      <h2>{% translate "Cancel your recurring donations" %}</h2>
+      <p>{% translate "You can cancel your recurring donation to the Django Software Foundation anytime." %}</p>
       <ul>
         {% for donation in recurring_donations %}
           <li>
-            Your {{ donation.interval }} recurring donation of ${{ donation.subscription_amount }}.
+            {% blocktranslate trimmed with interval=donation.interval amount=donation.subscription_amount %}
+              Your {{ interval }} recurring donation of ${{ amount }}.
+            {% endblocktranslate %}
             <form method="POST" action="{% url 'fundraising:cancel-donation' hero.pk %}">
               {% csrf_token %}
               <input name="donation" type="hidden" value="{{ donation.pk }}">
-              <input type="submit" value="cancel this donation">
+              <input type="submit" value="{% translate "cancel this donation" %}">
             </form>
           </li>
         {% endfor %}
@@ -67,11 +73,11 @@
     {% endif %}
     {% if past_payments %}
       <hr />
-      <h2>Your past donations</h2>
+      <h2>{% translate "Your past donations" %}</h2>
       <ul>
         {% for payment in past_payments %}
           <li>
-            ${{ payment.amount }} on {{ payment.date|date:"N jS, Y \a\t P" }}
+            ${{ payment.amount }} on {{ payment.date|date:"DATETIME_FORMAT" }}
             ({{ payment.donation.get_interval_display }})
           </li>
         {% endfor %}

djangoproject/templates/fundraising/thank-you.html
@@ -1,30 +1,45 @@
 {% extends 'fundraising/index.html' %}
-{% load humanize static %}
+{% load humanize i18n static %}
 
 {% block content %}
-  <h1>Thank you for supporting the Django Project!</h1>
-  <p>Your support is <strong>invaluable</strong> to continue the rapid development
-    of Django and helps the <strong>Django Fellowship</strong> program in particular.</p>
+  <h1>{% translate "Thank you for supporting the Django Project!" %}</h1>
+  <p>
+    {% blocktranslate trimmed %}
+      Your support is <strong>invaluable</strong> to continue the rapid development
+      of Django and helps the <strong>Django Fellowship</strong> program in particular.
+    {% endblocktranslate %}
+  </p>
 
-  <p>Please help us spread the word and encourage your friends and colleagues to
-    donate. Thank you!</p>
+  <p>
+    {% blocktranslate trimmed %}
+      Please help us spread the word and encourage your friends and colleagues to
+      donate. Thank you!
+    {% endblocktranslate %}
+  </p>
 
   <a href="https://twitter.com/intent/tweet?button_hashtag=SupportDjango&text=I+just+donated+to+support+the+Django+Software+Foundation.+Join+me!+https%3A%2F%2Fwww.djangoproject.com%2Ffundraising%2F"
      class="twitter-hashtag-button" data-related="djangoproject">Tweet #SupportDjango</a>
   <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
   <p>
-    You should receive an email shortly, with confirmation of your donation,
-    details of badges you can display to show you support of Django, managing your
-    donation,  and how you can have your name or company displayed on the
-    Fundraising page and in the sidebar on the Django Project website.
+    {% blocktranslate trimmed %}
+      You should receive an email shortly, with confirmation of your donation,
+      details of badges you can display to show you support of Django, managing your
+      donation,  and how you can have your name or company displayed on the
+      Fundraising page and in the sidebar on the Django Project website.
+    {% endblocktranslate %}
   </p>
 
-  <p>Welcome aboard! ⛵️</p>
+  <p>{% translate "Welcome aboard! ⛵️" %}</p>
 
   <hr>
 
-  <p>If you have any <strong>problems</strong> with your donation please email <a href="mailto:donations@djangoproject.com">donations@djangoproject.com</a> for help.</p>
+  <p>
+    {% blocktranslate trimmed %}
+      If you have any <strong>problems</strong> with your donation please email
+      <a href="mailto:donations@djangoproject.com">donations@djangoproject.com</a> for help.
+    {% endblocktranslate %}
+  </p>
 
 {% endblock %}
 

commit 6f05eff04c3d70fd6610f24d88b4104fc6a7930e

Fixed crash in fundraising checkout view
The crash was introduced by a41119e30356a4147b4fb857e83b6c51a931fac4

fundraising/views.py
@@ -298,7 +298,7 @@ class WebhookHandler:
         customer = stripe.Customer.retrieve(
             session.customer, stripe_version="2020-08-27"
         )
-        hero, _ = DjangoHero.objects.get_or_create(
+        hero, _created = DjangoHero.objects.get_or_create(
             stripe_customer_id=customer.id,
             defaults={
                 "email": customer.email,