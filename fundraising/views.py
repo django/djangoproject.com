2f7f0696d395616492d5f3b15a5b33948295baeb
Arpan Dey <69454099+arpan8925@users.noreply.github.com>
2024-11-25 23:47:04 +0600

    Update views.py

--- fundraising/views.py
+++ fundraising/views.py
@@ -16,7 +16,6 @@ from django.utils.translation import gettext as _
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from stripe.error import InvalidRequestError
 
 from .forms import DjangoHeroForm, DonationForm, PaymentForm
 from .models import DjangoHero, Donation, Payment, Testimonial
