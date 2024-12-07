30322026dba9f7f180df50272fc90ba8231c5533
Baptiste Mispelon <bmispelon@gmail.com>
2024-12-07 15:16:32 +0100

    Improved Release model to support PEP625 changes.
    Three new file fields (tarball, wheel, checksum) were added, making
    the link generation to these files more robust to changes like the
    one mandated by PEP625 (changing of file case).
    
    A new is_active boolean field was also added, making it easier
    to start work on a release without it being public.

--- releases/admin.py
+++ releases/admin.py
@@ -5,8 +5,14 @@ from .models import Release
 
 @admin.register(Release)
 class ReleaseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["version", "is_active", "is_lts"]}),
+        ("Dates", {"fields": ["date", "eol_date"]}),
+        ("Artifacts", {"fields": ["tarball", "wheel", "checksum"]}),
+    ]
     list_display = (
         "version",
+        "show_is_published",
         "is_lts",
         "date",
         "eol_date",
@@ -16,8 +22,18 @@ class ReleaseAdmin(admin.ModelAdmin):
         "show_status",
         "iteration",
     )
-    list_filter = ("status", "is_lts")
+    list_filter = ("status", "is_lts", "is_active")
     ordering = ("-major", "-minor", "-micro", "-status", "-iteration")
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form_class = super().get_form(request, obj=obj, change=change, **kwargs)
+        # Add `accept` attributes to the artifact file fields to make it a bit
+        # easier to pick the right files in the browser's 'filepicker
+        extensions = {"tarball": ".tar.gz", "wheel": ".whl", "checksum": ".asc,.txt"}
+        for field, accept in extensions.items():
+            widget = form_class.base_fields[field].widget
+            widget.attrs.setdefault("accept", accept)
+        return form_class
 
     @admin.display(
         description="status",
@@ -25,3 +41,11 @@ class ReleaseAdmin(admin.ModelAdmin):
     )
     def show_status(self, obj):
         return obj.get_status_display()
+
+    @admin.display(
+        boolean=True,
+        description="Published?",
+        ordering="is_active",
+    )
+    def show_is_published(self, obj):
+        return obj.is_published
