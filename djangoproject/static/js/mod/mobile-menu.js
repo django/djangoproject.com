4ef887b305acdda33e5b0459d1802987b92cf2fa
Adam Zapletal <adamzap@gmail.com>
2024-12-20 14:48:08 -0600

    Run `prettier` on JavaScript via `pre-commit`
    This patch removes the global `djangoproject/static/*` exclude rule in
    `.pre-commit-config.yaml` to enable `prettier` to run on this project's
    JavaScript files. The `djangoproject/static/js/lib/` directory is now
    excluded in the `prettier` hook because there is no value in
    reformatting a minified JavaScript file.
    
    I also added a `.prettierrc` file to the root of the project to control
    JavaScript formatting.
    
    Refs #1827

--- djangoproject/static/js/mod/mobile-menu.js
+++ djangoproject/static/js/mod/mobile-menu.js
@@ -1,23 +1,22 @@
 define([
-    'jquery' //requires jquery
-], function( $ ) {
+  'jquery', //requires jquery
+], function ($) {
+  var MobileMenuExport = function (menu) {
+    this.menu = $(menu); //menu container
+    this.toggleMenuBtn = $('.menu-button');
+    this.init();
+  };
 
-    var MobileMenuExport = function(menu) {
-        this.menu = $(menu); //menu container
-        this.toggleMenuBtn = $('.menu-button')
-        this.init();
-    };
+  MobileMenuExport.prototype = {
+    init: function () {
+      var self = this;
+      self.toggleMenuBtn.on('click', function () {
+        self.menu.toggleClass('active');
+        self.toggleMenuBtn.toggleClass('active');
+      });
+    },
+  };
 
-    MobileMenuExport.prototype = {
-        init: function(){
-            var self = this;
-            self.toggleMenuBtn.on( 'click', function(){
-                self.menu.toggleClass('active');
-                self.toggleMenuBtn.toggleClass('active');
-            });
-        }
-    };
-
-    // Export a single instance of our module:
-    return new MobileMenuExport('[role="banner"] [role="navigation"]');
+  // Export a single instance of our module:
+  return new MobileMenuExport('[role="banner"] [role="navigation"]');
 });
--- djangoproject/static/js/mod/stripe-donation.js
+++ djangoproject/static/js/mod/stripe-donation.js
@@ -1,59 +1,58 @@
 define([
-    'jquery', //requires
-    'stripe'
-], function($) {
-    var $donationForm = $('.stripe-donation');
+  'jquery', //requires
+  'stripe',
+], function ($) {
+  var $donationForm = $('.stripe-donation');
 
-    function postToStripe(recaptchaToken) {
-        var interval = $donationForm.find('[name=interval]').val();
-        var amount = $donationForm.find('[name=amount]').val();
-        var csrfToken = $donationForm.find('[name=csrfmiddlewaretoken]').val();
-        var data = {
-            'interval': interval,
-            'amount': amount,
-            'captcha': recaptchaToken,
-            'csrfmiddlewaretoken': csrfToken
-        }
-        $.ajax({
-            type: "POST",
-            url: $donationForm.attr('action-for-donation-session'),
-            data: data,
-            dataType: 'json',
-            success: function (data) {
-                console.log(data)
-                if (data.success) {
-                    var stripe = Stripe($donationForm.data('stripeKey'))
-                    return stripe.redirectToCheckout({sessionId: data.sessionId})
-                } else {
-                    msg = 'There was an error setting up your donation. '
-                    if (data.error.amount) {
-                        msg += data.error.amount
-                    } else {
-                        msg += 'Sorry. Please refresh the page and try again.'
-                    }
-                    alert(msg);
-                }
-            }
-        })
+  function postToStripe(recaptchaToken) {
+    var interval = $donationForm.find('[name=interval]').val();
+    var amount = $donationForm.find('[name=amount]').val();
+    var csrfToken = $donationForm.find('[name=csrfmiddlewaretoken]').val();
+    var data = {
+      interval: interval,
+      amount: amount,
+      captcha: recaptchaToken,
+      csrfmiddlewaretoken: csrfToken,
     };
-
-    // django-recaptcha==4.0.0 adds a `submit` event listener to the form that
-    // ends up calling form.submit(), therefore bypassing our own event listener.
-    // As a workaround, we remove their event listener and replace it with our own.
-    if (window.recaptchaFormSubmit !== undefined) {
-        $donationForm[0].removeEventListener("submit", window.recaptchaFormSubmit);
-    }
-    $donationForm.on('submit', function (e) {
-        e.preventDefault();
-        let captcha_input = document.getElementById("id_captcha"),
-            public_key = captcha_input.getAttribute('data-sitekey');
-        // Validate token on form submit.
-        // NOTE: the `action` key must match the one defined on the widget.
-        grecaptcha.execute(public_key, {action: 'form'}).then(function(token) {
-            captcha_input.value = token;
-            console.log("reCAPTCHA validated. Posting to stripe...");
-            postToStripe(token);
-        });
+    $.ajax({
+      type: 'POST',
+      url: $donationForm.attr('action-for-donation-session'),
+      data: data,
+      dataType: 'json',
+      success: function (data) {
+        console.log(data);
+        if (data.success) {
+          var stripe = Stripe($donationForm.data('stripeKey'));
+          return stripe.redirectToCheckout({ sessionId: data.sessionId });
+        } else {
+          msg = 'There was an error setting up your donation. ';
+          if (data.error.amount) {
+            msg += data.error.amount;
+          } else {
+            msg += 'Sorry. Please refresh the page and try again.';
+          }
+          alert(msg);
+        }
+      },
     });
+  }
 
+  // django-recaptcha==4.0.0 adds a `submit` event listener to the form that
+  // ends up calling form.submit(), therefore bypassing our own event listener.
+  // As a workaround, we remove their event listener and replace it with our own.
+  if (window.recaptchaFormSubmit !== undefined) {
+    $donationForm[0].removeEventListener('submit', window.recaptchaFormSubmit);
+  }
+  $donationForm.on('submit', function (e) {
+    e.preventDefault();
+    let captcha_input = document.getElementById('id_captcha'),
+      public_key = captcha_input.getAttribute('data-sitekey');
+    // Validate token on form submit.
+    // NOTE: the `action` key must match the one defined on the widget.
+    grecaptcha.execute(public_key, { action: 'form' }).then(function (token) {
+      captcha_input.value = token;
+      console.log('reCAPTCHA validated. Posting to stripe...');
+      postToStripe(token);
+    });
+  });
 });
