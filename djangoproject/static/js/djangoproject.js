69b131c4097bb8c18cc8cd77fee797b7442ac3e0
Adam Zapletal <adamzap@gmail.com>
2024-12-20 16:45:22 -0600

    Remove obvious part of comment

--- djangoproject/static/js/djangoproject.js
+++ djangoproject/static/js/djangoproject.js
@@ -1,5 +1,4 @@
-// Toggle persistent display of documentation version and language options if
-// the element is clicked
+// Toggle persistent display of documentation version and language options
 document.querySelectorAll('.doc-switcher li.current').forEach(function (el) {
   el.addEventListener('click', function () {
     this.parentElement.classList.toggle('open');
