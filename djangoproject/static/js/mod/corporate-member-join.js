6dc8f418e6b652863ccb16a3e2e01795e8a89d9f
Adam Zapletal <adamzap@gmail.com>
2024-12-20 14:48:08 -0600

    Run `prettier` on JavaScript via `pre-commit`
    This patch removes the global `djangoproject/static/*` exclude rule in
    `.pre-commit-config.yaml` to enable `prettier` to run on this project's
    JavaScript files. The `djangoproject/static/js/lib/` directory is now
    excluded in the `prettier` hook because there is no value in
    reformatting a minified JavaScript file.
    
    I also added a `.prettierrc` file to the root of the project to control
    JavaScript formatting.
    
    Refs #1827

--- djangoproject/static/js/mod/corporate-member-join.js
+++ djangoproject/static/js/mod/corporate-member-join.js
@@ -1,52 +1,52 @@
-define(['jquery'], function($) {
-    var CorporateMembershipJoinForm = function(form) {
-        this.form = $(form);
-        this.init();
-    };
-    CorporateMembershipJoinForm.prototype = {
-        init: function() {
-            var self = this;
-            var $amount = self.form.find('#id_amount');
-            var $membershipLevel = self.form.find('#id_membership_level');
-            $amount.change(function() {
-                self.setMembershipLevel($(this), $membershipLevel);
-            });
-            $membershipLevel.change(function() {
-                self.setDonationAmount($(this), $amount);
-            });
-        },
-        setDonationAmount: function($membershipLevel, $amount) {
-            var selectedMembership = $membershipLevel.val();
-            if (selectedMembership == '5') {
-                $amount.val(100000);
-            } else if (selectedMembership == '4') {
-                $amount.val(30000);
-            } else if (selectedMembership == '3') {
-                $amount.val(12500);
-            } else if (selectedMembership == '2') {
-                $amount.val(5000);
-            } else if (selectedMembership == '1') {
-                $amount.val(2000);
-            } else {
-                $amount.val('');
-            }
-        },
-        setMembershipLevel: function($amount, $membershipLevel) {
-            var amount = parseInt($amount.val());
-            if (amount >= 100000) {
-                $membershipLevel.val(5);
-            } else if (amount >= 30000) {
-                $membershipLevel.val(4);
-            } else if (amount >= 12500) {
-                $membershipLevel.val(3);
-            } else if (amount >= 5000) {
-                $membershipLevel.val(2);
-            } else if (amount >= 2000) {
-                $membershipLevel.val(1);
-            } else {
-                $membershipLevel.val('');
-            }
-        },
-    };
-    return new CorporateMembershipJoinForm('.corporate-membership-join-form');
+define(['jquery'], function ($) {
+  var CorporateMembershipJoinForm = function (form) {
+    this.form = $(form);
+    this.init();
+  };
+  CorporateMembershipJoinForm.prototype = {
+    init: function () {
+      var self = this;
+      var $amount = self.form.find('#id_amount');
+      var $membershipLevel = self.form.find('#id_membership_level');
+      $amount.change(function () {
+        self.setMembershipLevel($(this), $membershipLevel);
+      });
+      $membershipLevel.change(function () {
+        self.setDonationAmount($(this), $amount);
+      });
+    },
+    setDonationAmount: function ($membershipLevel, $amount) {
+      var selectedMembership = $membershipLevel.val();
+      if (selectedMembership == '5') {
+        $amount.val(100000);
+      } else if (selectedMembership == '4') {
+        $amount.val(30000);
+      } else if (selectedMembership == '3') {
+        $amount.val(12500);
+      } else if (selectedMembership == '2') {
+        $amount.val(5000);
+      } else if (selectedMembership == '1') {
+        $amount.val(2000);
+      } else {
+        $amount.val('');
+      }
+    },
+    setMembershipLevel: function ($amount, $membershipLevel) {
+      var amount = parseInt($amount.val());
+      if (amount >= 100000) {
+        $membershipLevel.val(5);
+      } else if (amount >= 30000) {
+        $membershipLevel.val(4);
+      } else if (amount >= 12500) {
+        $membershipLevel.val(3);
+      } else if (amount >= 5000) {
+        $membershipLevel.val(2);
+      } else if (amount >= 2000) {
+        $membershipLevel.val(1);
+      } else {
+        $membershipLevel.val('');
+      }
+    },
+  };
+  return new CorporateMembershipJoinForm('.corporate-membership-join-form');
 });
--- djangoproject/static/js/mod/search-key.js
+++ djangoproject/static/js/mod/search-key.js
@@ -1,31 +1,42 @@
 define([
-    'jquery' //requires jquery
-], function( $ ) {
-    const SearchForm = function(search_form) {
-        this.search_form = $(search_form); // the search form
-        this.init();
-    };
+  'jquery', //requires jquery
+], function ($) {
+  const SearchForm = function (search_form) {
+    this.search_form = $(search_form); // the search form
+    this.init();
+  };
 
-    SearchForm.prototype = {
-        init: function(){
-            const self = this;
-            $(document).ready(function () {
-                const search_form_input = self.search_form.find('input');
-                const raw_placeholder = search_form_input.attr('placeholder');
-                const shortcut = navigator.userAgent.indexOf("Mac") === -1 ? "Ctrl + K" : "⌘ + K";
-                search_form_input.attr('placeholder', `${raw_placeholder} (${shortcut})`);
+  SearchForm.prototype = {
+    init: function () {
+      const self = this;
+      $(document).ready(function () {
+        const search_form_input = self.search_form.find('input');
+        const raw_placeholder = search_form_input.attr('placeholder');
+        const shortcut =
+          navigator.userAgent.indexOf('Mac') === -1 ? 'Ctrl + K' : '⌘ + K';
+        search_form_input.attr(
+          'placeholder',
+          `${raw_placeholder} (${shortcut})`,
+        );
 
-                $(window).keydown(function(e) {
-                    if ((e.metaKey || e.ctrlKey) && e.key === 'k' && $('input:focus, textarea:focus').length === 0) {
-                        search_form_input.focus().select();
-                        search_form_input[0].scrollIntoView({ behavior: "smooth", block: "start" });
-                        return false;
-                    }
-                });
+        $(window).keydown(function (e) {
+          if (
+            (e.metaKey || e.ctrlKey) &&
+            e.key === 'k' &&
+            $('input:focus, textarea:focus').length === 0
+          ) {
+            search_form_input.focus().select();
+            search_form_input[0].scrollIntoView({
+              behavior: 'smooth',
+              block: 'start',
             });
-        }
-    };
+            return false;
+          }
+        });
+      });
+    },
+  };
 
-    // Export a single instance of our module:
-    return new SearchForm('.search');
+  // Export a single instance of our module:
+  return new SearchForm('.search');
 });
