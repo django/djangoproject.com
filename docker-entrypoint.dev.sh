1bb286daa80cb8b5d5cc863e711499d3c30e60a3
Paolo Melchiorre <paolo@melchiorre.org>
2025-01-20 12:43:35 +0100

    Move db healtcheck in docker compose

--- docker-entrypoint.dev.sh
+++ docker-entrypoint.dev.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
-
-while ! pg_isready -h db -p 5432 -q; do
-  sleep 0.1
-done
-
-echo "PostgreSQL started"
-
 python -m manage flush --no-input
 # PGPASSWORD=djangoproject psql --host db --port 5432 --username=code.djangoproject --dbname=code.djangoproject < tracdb/trac.sql
 python -m manage migrate
