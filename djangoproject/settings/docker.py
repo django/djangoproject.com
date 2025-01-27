e6c11b9cf48d8e577fb0f81a7180585b2c95ad7b
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-01-27 15:31:52 +0300

    Use .dev as base in settings/docker.py

--- djangoproject/settings/docker.py
+++ djangoproject/settings/docker.py
@@ -1,17 +1,4 @@
-from .common import *  # noqa
-from .dev import (  # noqa
-    CACHES,
-    CSRF_COOKIE_SECURE,
-    DEBUG,
-    DOCS_BUILD_ROOT,
-    EMAIL_BACKEND,
-    MEDIA_ROOT,
-    PUSH_SSL_CALLBACK,
-    SESSION_COOKIE_SECURE,
-    SILENCED_SYSTEM_CHECKS,
-    STATIC_ROOT,
-    THUMBNAIL_DEBUG,
-)
+from .dev import *  # noqa: F403
 
 DATABASES = {
     "default": {
@@ -32,16 +19,3 @@ LOCALE_MIDDLEWARE_EXCLUDED_HOSTS = ["docs.djangoproject.localhost"]
 
 # django-hosts settings
 PARENT_HOST = "localhost:8000"
-
-# Enable optional components
-if DEBUG:
-    INSTALLED_APPS.append("debug_toolbar")
-    INTERNAL_IPS = ["127.0.0.1"]
-    MIDDLEWARE.insert(
-        MIDDLEWARE.index("django.middleware.common.CommonMiddleware") + 1,
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
-    )
-    MIDDLEWARE.insert(
-        MIDDLEWARE.index("debug_toolbar.middleware.DebugToolbarMiddleware") + 1,
-        "djangoproject.middleware.CORSMiddleware",
-    )
