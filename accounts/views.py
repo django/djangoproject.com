8a612b3af61de9c3f6d4534c04cc756e1a52dad2
Adam Zapletal <adamzap@gmail.com>
2025-06-03 09:11:24 -0500

    Simplified cache usage in `accounts` view helper
    Importing `cache` instead of `caches` uses the default cache.

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ import hashlib
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.core.cache import caches
+from django.core.cache import cache
 from django.shortcuts import get_object_or_404, redirect, render
 
 from tracdb import stats as trac_stats
@@ -35,10 +35,9 @@ def edit_profile(request):
 
 
 def get_user_stats(user):
-    c = caches["default"]
     username = user.username.encode("ascii", "ignore")
     key = "user_vital_status:%s" % hashlib.md5(username).hexdigest()
-    info = c.get(key)
+    info = cache.get(key)
     if info is None:
         info = trac_stats.get_user_stats(user.username)
         # Hide any stat with a value = 0 so that we don't accidentally insult
@@ -46,5 +45,5 @@ def get_user_stats(user):
         for k, v in list(info.items()):
             if v.count == 0:
                 info.pop(k)
-        c.set(key, info, 60 * 60)
+        cache.set(key, info, 60 * 60)
     return info
