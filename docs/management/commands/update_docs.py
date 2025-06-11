8b35e1aa3a72ebdb8b82d5705e681efcddc9ac9d
alexgmin <8340441+alexgmin@users.noreply.github.com>
2025-06-11 14:38:15 +0200

    Speed up initial docs cloning

--- docs/management/commands/update_docs.py
+++ docs/management/commands/update_docs.py
@@ -358,7 +358,7 @@ class Command(BaseCommand):
                 os.chdir(cwd)
         else:
             subprocess.check_call(
-                ["git", "clone", "--branch", branch, repo, str(destdir), quiet],
+                ["git", "clone", "--depth", "1", "--branch", branch, repo, str(destdir), quiet],
                 stderr=sys.stdout,
             )
         return True
