5aa679c00e701115b90823c8dc5c13c871817e83
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-07-27 19:51:41 +0300

    Apply black to migrations

--- foundation/migrations/0006_hardcode_currency_choices.py
+++ foundation/migrations/0006_hardcode_currency_choices.py
@@ -9,23 +9,49 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('foundation', '0005_alter_approvedgrant_amount_currency_and_more'),
+        ("foundation", "0005_alter_approvedgrant_amount_currency_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='approvedgrant',
-            name='amount',
-            field=djmoney.models.fields.MoneyField(currency_choices=[('AUD', 'Australian Dollar'), ('EUR', 'Euro'), ('NGN', 'Nigerian Naira'), ('USD', 'US Dollar')], decimal_places=2, default=Decimal('0.0'), default_currency='USD', max_digits=10),
+            model_name="approvedgrant",
+            name="amount",
+            field=djmoney.models.fields.MoneyField(
+                currency_choices=[
+                    ("AUD", "Australian Dollar"),
+                    ("EUR", "Euro"),
+                    ("NGN", "Nigerian Naira"),
+                    ("USD", "US Dollar"),
+                ],
+                decimal_places=2,
+                default=Decimal("0.0"),
+                default_currency="USD",
+                max_digits=10,
+            ),
         ),
         migrations.AlterField(
-            model_name='approvedgrant',
-            name='amount_currency',
-            field=djmoney.models.fields.CurrencyField(choices=[('AUD', 'Australian Dollar'), ('EUR', 'Euro'), ('NGN', 'Nigerian Naira'), ('USD', 'US Dollar')], default='USD', editable=False, max_length=3),
+            model_name="approvedgrant",
+            name="amount_currency",
+            field=djmoney.models.fields.CurrencyField(
+                choices=[
+                    ("AUD", "Australian Dollar"),
+                    ("EUR", "Euro"),
+                    ("NGN", "Nigerian Naira"),
+                    ("USD", "US Dollar"),
+                ],
+                default="USD",
+                editable=False,
+                max_length=3,
+            ),
         ),
         migrations.AlterField(
-            model_name='meeting',
-            name='treasurer_balance_currency',
-            field=djmoney.models.fields.CurrencyField(choices=[('USD', 'US Dollar')], default='USD', editable=False, max_length=3),
+            model_name="meeting",
+            name="treasurer_balance_currency",
+            field=djmoney.models.fields.CurrencyField(
+                choices=[("USD", "US Dollar")],
+                default="USD",
+                editable=False,
+                max_length=3,
+            ),
         ),
     ]
