ff916bcf31f50c71b8d779a7217b7025d1454f7d
Eric Sherman <ericandrewsherman@gmail.com>
2025-09-12 13:16:09 -0500

    Map tracdb specific values to config and compose.yml
    This could probably be refactored, but is needed so that tests and other
    code that use the tracsb have expected credentials needed inside the container

--- djangoproject/settings/docker.py
+++ djangoproject/settings/docker.py
@@ -10,6 +10,15 @@ DATABASES = {
         "PORT": os.environ.get("SQL_PORT"),
     }
 }
+# Trac connection
+DATABASES["trac"] = {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.environ.get("TRAC_DATABASE", "code.djangoproject"),
+    "USER": os.environ.get("TRAC_USER", "code.djangoproject"),
+    "PASSWORD": os.environ.get("TRAC_PASSWORD", ""),
+    "HOST": os.environ.get("TRAC_HOST", "db"),
+    "PORT": os.environ.get("TRAC_PORT", "5432"),
+}
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
