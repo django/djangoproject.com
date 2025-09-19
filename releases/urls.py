eb3a69af9fa9f86b5784638aecfa0f486e1c1bbc
Natalia <124304+nessita@users.noreply.github.com>
2025-09-18 22:07:37 -0300

    Automatically generate Django series roadmaps from Release data
    This branch adds a new view that replaces the Roadmap wiki pages, e.g.:
    
      * https://code.djangoproject.com/wiki/Version5.2Roadmap
      * https://code.djangoproject.com/wiki/Version6.0Roadmap
      * https://code.djangoproject.com/wiki/Version6.1Roadmap
    
    The wiki pages were almost identical except for version-specific details
    and target dates. This information is now generated dynamically from the
    Release model in the database.
    
    The supported and future Roadmap tables were reorganized so they follow
    a more intuitive timeline (from older to newer versions).

--- releases/urls.py
+++ releases/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, re_path
 
-from .views import index, redirect
+from .views import index, redirect, roadmap
 
 urlpatterns = [
     path("", index, name="download"),
     re_path(
         "^([0-9a-z_.-]+)/(tarball|wheel|checksum)/$", redirect, name="download-redirect"
     ),
+    re_path(r"^(?P<series>\d{1,2}\.[0-2])/roadmap/$", roadmap, name="roadmap"),
 ]
