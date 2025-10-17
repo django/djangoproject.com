550da09ece343895580be0d9c7a3c00def3524f1
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2025-10-17 18:54:09 +0200

    Added DEFAULT_LANGUAGE_CODE setting.

--- docs/models.py
+++ docs/models.py
@@ -39,9 +39,9 @@ from .search import (
 
 
 class DocumentReleaseQuerySet(models.QuerySet):
-    def current(self, lang="en"):
+    def current(self, lang=settings.DEFAULT_LANGUAGE_CODE):
         current = self.get(is_default=True)
-        if lang != "en":
+        if lang != settings.DEFAULT_LANGUAGE_CODE:
             try:
                 return self.get(lang=lang, release=current.release)
             except DocumentRelease.DoesNotExist:
@@ -91,7 +91,9 @@ class DocumentRelease(models.Model):
 
     DEFAULT_CACHE_KEY = "%s_docs_version" % settings.CACHE_MIDDLEWARE_KEY_PREFIX
 
-    lang = models.CharField(max_length=7, choices=settings.LANGUAGES, default="en")
+    lang = models.CharField(
+        max_length=7, choices=settings.LANGUAGES, default=settings.DEFAULT_LANGUAGE_CODE
+    )
     release = models.ForeignKey(
         Release,
         null=True,
