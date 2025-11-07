511c9224b462d570ebc85447cca166fd3a04d324
Ertuğrul Keremoğlu <ertugkeremoglu@gmail.com>
2025-11-07 18:20:47 +0300

    Add tests for the management command `link_individual_members_to_users_by_email`

--- members/test_management.py
+++ members/test_management.py
@@ -2,6 +2,7 @@ from datetime import date, timedelta
 from io import StringIO
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core import mail
 from django.core.management import call_command
 from django.test import TestCase
@@ -14,8 +15,70 @@ from members.models import (
     IndividualMemberAccountInviteSendMailStatus,
 )
 
+User = get_user_model()
+
 
 class IndividualMemberTests(TestCase):
+    def test_link_individual_members_to_users_by_email(self):
+        members_count = 4
+        user_pk_to_email_mapping = {}
+        for i in range(members_count):
+            username = f"user{i}"
+            email = f"{username}@example.com"
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password="password",
+            )
+            user_pk_to_email_mapping[user.pk] = email
+            IndividualMember.objects.create(
+                name=f"User {i}",
+                email=email,
+            )
+        self.assertEqual(
+            IndividualMember.objects.filter(
+                user_id__isnull=True,
+                email__in=user_pk_to_email_mapping.values(),
+            ).count(),
+            members_count,
+        )
+        stdout = StringIO()
+        stderr = StringIO()
+        call_command(
+            "link_individual_members_to_users_by_email",
+            stdout=stdout,
+            stderr=stderr,
+        )
+        individual_member_queryset = IndividualMember.objects.filter(
+            user_id__in=user_pk_to_email_mapping.keys(),
+        )
+        self.assertEqual(
+            individual_member_queryset.count(),
+            members_count,
+        )
+        for individual_member in individual_member_queryset.iterator():
+            self.assertEqual(
+                individual_member.email,
+                user_pk_to_email_mapping[individual_member.user_id],
+            )
+        stdout_output_lines = stdout.getvalue().splitlines()
+        self.assertIn(
+            f"Updated: {members_count}",
+            stdout_output_lines,
+        )
+
+    def test_link_individual_members_to_users_by_email_with_option_no_logging(self):
+        stdout = StringIO()
+        stderr = StringIO()
+        call_command(
+            "link_individual_members_to_users_by_email",
+            no_logging=True,
+            stdout=stdout,
+            stderr=stderr,
+        )
+        self.assertEqual(len(stdout.getvalue()), 0)
+        self.assertEqual(len(stderr.getvalue()), 0)
+
     def test_send_individual_member_account_invite_mails(self):
         IndividualMember.objects.create(
             name="Member 1",
