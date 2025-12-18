5d8e73dc1b9f747e154cd4a39a3f91c0586f018d
ahmedasar00 <ahmdasartech@outlook.com>
2025-12-18 23:06:29 +0200

    Simplify Markdown tables extension usage

--- blog/models.py
+++ blog/models.py
@@ -69,8 +69,7 @@ class ContentFormat(models.TextChoices):
                 extensions=[
                     # baselevel matches `initial_header_level` from BLOG_DOCUTILS_SETTINGS
                     TocExtension(baselevel=3, slugify=_md_slugify),
-                    "markdown.extensions.tables",
-                    TableExtension(),
+                    "tables",
                 ],
             )
         raise ValueError(f"Unsupported format {fmt}")
