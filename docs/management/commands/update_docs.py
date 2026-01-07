131dcf636c329b949464e456f883ff67e031d0b1
keha <keha.chandrakar@9envoy.com>
2026-01-07 16:18:26 +0530

    Run Sphinx builds in isolated subprocesses

--- docs/management/commands/update_docs.py
+++ docs/management/commands/update_docs.py
@@ -189,43 +189,31 @@ class Command(BaseCommand):
         # Use Sphinx to build the release docs into JSON and HTML documents.
         #
         for builder in builders:
-            # Wipe and re-create the build directory. See #18930.
             build_dir = parent_build_dir / "_build" / builder
             if build_dir.exists():
-                shutil.rmtree(str(build_dir))
+                shutil.rmtree(build_dir)
             build_dir.mkdir(parents=True)
 
-            if self.verbosity >= 2:
-                self.stdout.write(f"  building {builder} ({source_dir} -> {build_dir})")
-            # Retrieve the extensions from the conf.py so we can append to them.
+            doctreedir = parent_build_dir / "_doctrees" / release.lang / builder
+            if doctreedir.exists():
+                shutil.rmtree(doctreedir)
+            doctreedir.mkdir(parents=True)
+
             conf_extensions = Config.read(source_dir.resolve()).extensions
-            extensions = [*conf_extensions, "docs.builder"]
+            extensions = ",".join([*conf_extensions, "docs.builder"])
+
             try:
-                # Prevent global state persisting between builds
-                # https://github.com/sphinx-doc/sphinx/issues/12130
-                with patch_docutils(source_dir), docutils_namespace():
-                    Sphinx(
-                        srcdir=source_dir,
-                        confdir=source_dir,
-                        outdir=build_dir,
-                        doctreedir=build_dir / ".doctrees",
-                        buildername=builder,
-                        # Translated docs builds generate a lot of warnings, so send
-                        # stderr to stdout to be logged (rather than generating an email)
-                        warning=sys.stdout,
-                        parallel=multiprocessing.cpu_count(),
-                        verbosity=0,
-                        confoverrides={
-                            "language": to_locale(release.lang),
-                            "extensions": extensions,
-                        },
-                    ).build()
-                # Clean up global state after building each language.
-                _clean_up_global_state()
-            except SphinxError as e:
+                self.run_sphinx_build(
+                    source_dir=source_dir,
+                    build_dir=build_dir,
+                    doctreedir=doctreedir,
+                    builder=builder,
+                    language=to_locale(release.lang),
+                    extensions=extensions,
+                )
+            except subprocess.CalledProcessError as e:
                 self.stderr.write(
-                    "sphinx-build returned an error (release %s, builder %s): %s"
-                    % (release, builder, str(e))
+                    f"sphinx-build failed (release={release}, builder={builder}): {e}"
                 )
                 return
 
@@ -277,7 +265,30 @@ class Command(BaseCommand):
         json_built_dir = parent_build_dir / "_built" / "json"
         documents = gen_decoded_documents(json_built_dir)
         release.sync_to_db(documents)
-
+        def run_sphinx_build(self,*,source_dir,build_dir,doctreedir,builder,language,extensions,):
+            env = os.environ.copy()
+            env["SPHINXOPTS"] = " ".join(
+                [
+                    f"-D language={language}",
+                    f"-D extensions={extensions}",
+                ]
+            )
+            subprocess.check_call(
+                    [
+                    sys.executable,
+                    "-m",
+                    "sphinx",
+                    "-b",
+                    builder,
+                    "-c",
+                    str(source_dir),  
+                    "-d",
+                    str(doctreedir),
+                    str(source_dir),      
+                    str(build_dir),     
+                ],
+                env=env,
+            )
     def update_git(self, url, destdir, changed_dir="."):
         """
         Update a source checkout and return True if any docs were changed,
