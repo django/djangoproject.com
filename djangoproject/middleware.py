b14e40ed5bb2e91d6b48c44125024d53de6c6cb0
Vidhi Singh <vidhiii0909@gmail.com>
2026-01-28 19:35:10 +0530

    Normalize duplicate slashes in urls

--- djangoproject/middleware.py
+++ djangoproject/middleware.py
@@ -3,6 +3,8 @@ from django.http.request import split_domain_port
 from django.middleware.locale import LocaleMiddleware
 from django.utils.functional import cached_property
 from django.utils.http import is_same_domain
+from django.http import HttpResponsePermanentRedirect
+import re
 
 
 class CORSMiddleware:
@@ -50,3 +52,16 @@ class ExcludeHostsLocaleMiddleware(LocaleMiddleware):
         if self._is_host_included(request.get_host()):
             return super().process_response(request, response)
         return response
+
+class NormalizeSlashesMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        path = request.path
+        normalized = re.sub(r'/{2,}', '/', path)
+
+        if path != normalized:
+            return HttpResponsePermanentRedirect(normalized)
+
+        return self.get_response(request)
--- djangoproject/tests/test_www_urls.py
+++ djangoproject/tests/test_www_urls.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
-
+from django_hosts.resolvers import reverse
 
 class IndividualMembershipNominationRedirectTests(TestCase):
     def test_individual_membership_nomination_redirect_exists(self):
         response = self.client.get("/foundation/individual-membership-nomination/")
         self.assertEqual(response.status_code, 302)
+
+class NormalizeSlashesMiddlewareTests(TestCase):
+    def test_double_slash_redirects_to_single_slash(self):
+        response = self.client.get(
+            "/community//",
+            follow=True,
+            HTTP_HOST="www.djangoproject.localhost",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.request["PATH_INFO"].endswith("/community/"))
