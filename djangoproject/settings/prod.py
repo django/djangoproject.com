b2d4ebbc0c638f75d34904e1fbb70fbe99096a83
Md. Saikat Islam <saikatislam709@gmail.com>
2026-09-11 02:37:32 +0600

    Add optional S3 storage support

--- djangoproject/settings/prod.py
+++ djangoproject/settings/prod.py
@@ -52,14 +52,35 @@ MIDDLEWARE = (
 
 SESSION_COOKIE_SECURE = True
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
-    },
-}
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME") or SECRETS.get(
+    "aws_storage_bucket_name"
+)
+if AWS_STORAGE_BUCKET_NAME:
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        },
+    }
+    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME") or SECRETS.get(
+        "aws_s3_region_name"
+    )
+    if aws_s3_endpoint_url := (
+        os.getenv("AWS_S3_ENDPOINT_URL") or SECRETS.get("aws_s3_endpoint_url")
+    ):
+        AWS_S3_ENDPOINT_URL = aws_s3_endpoint_url
+    THUMBNAIL_STORAGE = STORAGES["default"]["BACKEND"]
+else:
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        },
+    }
 
 STATIC_ROOT = DATA_DIR / "static"
 
